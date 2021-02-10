ff317c277f3ad0c65735233e0b2274062a641daa
Alignof <neuromancer_wg@outlook.jp>
2021-02-11 02:51:49 +0900

    [add] enable to read file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
@@ -7,4 +9,11 @@ fn main() {
 	let filename = &args[2];
 	println!("searching for {}", query);
 	println!("In file {}", filename);
+
+	let mut f = File::open(filename).expect("file not found");
+
+	let mut contents = String::new();
+	f.read_to_string(&mut contents)
+		.expect("something went wrong reading the file");
+	println!("With text:\n {}", contents);
 }
