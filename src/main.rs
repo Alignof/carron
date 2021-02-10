21015f9560528a660bfa3399aa7453b77f921127
Alignof <neuromancer_wg@outlook.jp>
2021-02-11 02:41:18 +0900

    [add] add getting argument

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+	let args: Vec<String> = env::args().collect();
+
+	let query = &args[1];
+	let filename = &args[2];
+	println!("searching for {}", query);
+	println!("In file {}", filename);
 }
