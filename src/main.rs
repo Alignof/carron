a09712212a12a099d3190f1115475e25aafdd6bb
Alignof <neuromancer_wg@outlook.jp>
2021-02-13 22:55:20 +0900

    [update] update error handling

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use std::env;
+use std::process;
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
 	let args: Vec<String> = env::args().collect();
 
-	let args = Arguments::new(&args);
+	let args = Arguments::new(&args).unwrap_or_else(|err| {
+		println!("problem parsing arguments: {}", err);
+		process::exit(1);
+	});
+
 	println!("searching for {}", args.query);
 	println!("In file {}", args.filename);
 
-	let mut f = File::open(args.filename).expect("file not found");
+	let mut f = File::open(args.filename)
+		.expect("file not found");
 
 	let mut contents = String::new();
 	f.read_to_string(&mut contents)
@@ -23,11 +29,15 @@ struct Arguments{
 }
 
 impl Arguments{
-	fn new(args: &[String]) -> Arguments {
+	fn new(args: &[String]) -> Result<Arguments, &'static str> {
+		if args.len() < 3{
+			return Err("not enough arguments");
+		} 
+
 		let query = args[1].clone();
 		let filename = args[2].clone();
 
-		Arguments{query, filename}
+		Ok(Arguments{query, filename})
 	}
 }
 
