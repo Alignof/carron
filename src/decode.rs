533c64164999fc82b99990fb5632e6341953583f
Alignof <neuromancer_wg@outlook.jp>
2021-04-05 01:33:23 +0900

    [wip][add] add struct Instruction

--- src/decode.rs
+++ src/decode.rs
@@ -1,14 +1,7 @@
-pub trait Decode {
-	fn decode(&self, mmap: &[u8]) -> String;
-	fn decode_str(&self, mmap: &[u8]) -> String;
+struct Instruction {
+	op: OpecodeKind,
 }
 
-impl Decode for SectionHeader {
-	fn section_decode(&self, mmap: &[u8]){
-		for (block, dump_part) in (self.sh_offset .. self.sh_offset + self.sh_size as u32).step_by(4).enumerate(){
-			if block % 16 == 0 { println!() }
-			print!("{:08x} ", get_u32(mmap, dump_part as usize));
-		}
-		println!();
-	}
+pub trait Decode {
+	fn decode(&self, mmap: &[u8]) -> Instruction;
 }
