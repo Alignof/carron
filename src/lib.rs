d6e46f589ffa6a981e71d662fb21aa92429523f4
Alignof <neuromancer_wg@outlook.jp>
2021-04-13 10:17:51 +0900

    [add] add enum Option

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 pub mod elfload;
 pub mod decode;
 
+#[allow(non_camel_case_types)]
+enum Option {
+    OPT_ELFHEAD,
+    OPT_PROG,
+    OPT_SECT,
+    OPT_DISASEM,
+    OPT_DEFAULT,
+}
+
 pub struct Arguments {
 	pub arg_num: usize,
 	pub filename: String,
@@ -10,12 +19,15 @@ impl Arguments {
 	pub fn new(args: &[String]) -> Result<Arguments, &'static str> {
 		if args.len() < 2 {
 			return Err("not enough arguments");
-		}
+        }
 
 		let arg_num  = args.len();
 		let filename = args[1].clone();
 
-		Ok(Arguments { filename, arg_num })
+		Ok(Arguments {
+            filename,
+            arg_num,
+        })
 	}
 }
 
