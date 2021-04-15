92f8af83b7622f9f9b8e298051c40bf14cfa5105
Alignof <neuromancer_wg@outlook.jp>
2021-04-15 13:16:24 +0900

    [update] update the default option

--- src/main.rs
+++ src/main.rs
@@ -23,13 +23,9 @@ fn main() {
 		}
 	};
 
-	if loader.is_elf() {
+    if loader.is_elf() {
 		println!("elfcheck: OK");
 	}
-
-    if matches!(args.exe_option, ExeOption::OPT_ELFHEAD) {
-		println!("option -h");
-    }
 
     match args.exe_option {
         ExeOption::OPT_ELFHEAD	=> loader.ident_show(),
@@ -37,6 +33,6 @@ fn main() {
         ExeOption::OPT_SECT	    => loader.dump_section(),
         ExeOption::OPT_SHOWALL	=> loader.show_all_header(),
         ExeOption::OPT_DISASEM	=> loader.ident_show(),
-        ExeOption::OPT_DEFAULT	=> loader.ident_show(),
+        ExeOption::OPT_DEFAULT	=> loader.dump_section(),
     }
 }
