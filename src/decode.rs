d05b68a0567cf145d9417d5e30996c91cbc903bd
Alignof <neuromancer_wg@outlook.jp>
2021-04-19 09:35:48 +0900

    [update] add the converted string of compressed instruction

--- src/decode.rs
+++ src/decode.rs
@@ -86,54 +86,128 @@ pub struct Instruction {
     pub rs1: u8,
     pub rs2: u8,
     pub imm: u32,
+    pub is_compressed: bool,
 }
 
 impl Instruction {
     pub fn opc_to_string(&self) -> &'static str {
         use OpecodeKind::*;
         match self.opc {
-            OP_LUI		=> "lui",
-            OP_AUIPC	=> "auipc",
-            OP_JAL		=> "jal",
-            OP_JALR		=> "jalr",
-            OP_BEQ		=> "beq",
-            OP_BNE		=> "bne",
-            OP_BLT		=> "blt",
-            OP_BGE		=> "bge",
-            OP_BLTU		=> "bltu",
-            OP_BGEU		=> "bgeu",
-            OP_LB		=> "lb",
-            OP_LH		=> "lh",
-            OP_LW		=> "lw",
-            OP_LBU		=> "lbu",
-            OP_LHU		=> "lhu",
-            OP_SB		=> "sb",
-            OP_SH		=> "sh",
-            OP_SW		=> "sw",
-            OP_ADDI		=> "addi",
-            OP_SLTI		=> "slti",
-            OP_SLTIU	=> "sltiu",
-            OP_XORI		=> "xori",
-            OP_ORI		=> "ori",
-            OP_ANDI		=> "andi",
-            OP_SLLI		=> "slli",
-            OP_SRLI		=> "srli",
-            OP_ADD		=> "add",
-            OP_SUB		=> "sub",
-            OP_SLL		=> "sll",
-            OP_SLT		=> "slt",
-            OP_SLTU		=> "sltu",
-            OP_XOR		=> "xor",
-            OP_SRL		=> "srl",
-            OP_SRA		=> "sra",
-            OP_OR		=> "or",
-            OP_AND		=> "and",
-            OP_FENCE	=> "fence",
-            OP_ECALL	=> "ecall",
-            OP_EBREAK	=> "ebreak",
+            OP_LUI		    => "lui",
+            OP_AUIPC	    => "auipc",
+            OP_JAL		    => "jal",
+            OP_JALR		    => "jalr",
+            OP_BEQ		    => "beq",
+            OP_BNE		    => "bne",
+            OP_BLT		    => "blt",
+            OP_BGE		    => "bge",
+            OP_BLTU		    => "bltu",
+            OP_BGEU		    => "bgeu",
+            OP_LB		    => "lb",
+            OP_LH		    => "lh",
+            OP_LW		    => "lw",
+            OP_LBU		    => "lbu",
+            OP_LHU		    => "lhu",
+            OP_SB		    => "sb",
+            OP_SH		    => "sh",
+            OP_SW		    => "sw",
+            OP_ADDI		    => "addi",
+            OP_SLTI		    => "slti",
+            OP_SLTIU	    => "sltiu",
+            OP_XORI		    => "xori",
+            OP_ORI		    => "ori",
+            OP_ANDI		    => "andi",
+            OP_SLLI		    => "slli",
+            OP_SRLI		    => "srli",
+            OP_ADD		    => "add",
+            OP_SUB		    => "sub",
+            OP_SLL		    => "sll",
+            OP_SLT		    => "slt",
+            OP_SLTU		    => "sltu",
+            OP_XOR		    => "xor",
+            OP_SRL		    => "srl",
+            OP_SRA		    => "sra",
+            OP_OR		    => "or",
+            OP_AND		    => "and",
+            OP_FENCE	    => "fence",
+            OP_ECALL	    => "ecall",
+            OP_EBREAK	    => "ebreak",
+            OP_C_ADDI4SPN	=> "C.addi4spn",
+            OP_C_FLD		=> "C.fld",
+            OP_C_LW		    => "C.lw",
+            OP_C_FLW		=> "C.flw",
+            OP_C_FSD		=> "C.fsd",
+            OP_C_SW		    => "C.sw",
+            OP_C_FSW		=> "C.fsw",
+            OP_C_NOP		=> "C.nop",
+            OP_C_ADDI		=> "C.addi",
+            OP_C_JAL		=> "C.jal",
+            OP_C_LI		    => "C.li",
+            OP_C_ADDI16SP	=> "C.addi16sp",
+            OP_C_LUI		=> "C.lui",
+            OP_C_SRLI		=> "C.srli",
+            OP_C_SRAI		=> "C.srai",
+            OP_C_ANDI		=> "C.andi",
+            OP_C_SUB		=> "C.sub",
+            OP_C_XOR		=> "C.xor",
+            OP_C_OR		    => "C.or",
+            OP_C_AND		=> "C.and",
+            OP_C_J		    => "C.j",
+            OP_C_BEQZ		=> "C.beqz",
+            OP_C_BNEZ		=> "C.bnez",
+            OP_C_SLLI		=> "C.slli",
+            OP_C_FLDSP		=> "C.fldsp",
+            OP_C_JR		    => "C.jr",
+            OP_C_MV		    => "C.mv",
+            OP_C_EBREAK		=> "C.ebreak",
+            OP_C_JALR		=> "C.jalr",
+            OP_C_ADD		=> "C.add",
+            OP_C_FSDSP		=> "C.fsdsp",
+            OP_C_SWSP		=> "C.swsp",
+            OP_C_FSWSP		=> "C.fswsp",
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 pub trait Decode {
 	fn decode(&self, mmap: &[u8], index: usize) -> Instruction {
@@ -153,6 +227,7 @@ pub trait Decode {
             rs1: new_rs1,
             rs2: new_rs2,
             imm: new_imm,
+            is_compressed: true,
         }
     }
 }
