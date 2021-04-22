a0fb70b0fd8d8255b419cf8c277432f7f9bacab4
Alignof <neuromancer_wg@outlook.jp>
2021-04-22 09:41:59 +0900

    [add] add the function parse_rs1

--- src/decode/mmap_parse_16.rs
+++ src/decode/mmap_parse_16.rs
@@ -79,16 +79,18 @@ pub fn parse_opecode(inst:&u32) -> Result<OpecodeKind, &'static str> {
     }
 }
 
-pub fn parse_rd(inst: &u32, optype: OpecodeKind) -> u8 {
+pub fn parse_rd(inst: &u32, opkind: OpecodeKind) -> u8 {
     let q0_rd: u8 = ((inst >> 2) & 0x7) as u8;
     let q1_rd: u8 = ((inst >> 7) & 0x7) as u8;
     let q2_rd: u8 = ((inst >> 7) & 0x1F) as u8;
 
-    match optype {
+    match opkind {
+        // Quadrant 0
         OpecodeKind::OP_C_ADDI4SPN  => q0_rd,
         OpecodeKind::OP_C_FLD       => q0_rd,
         OpecodeKind::OP_C_FW        => q0_rd,
         OpecodeKind::OP_C_FLW       => q0_rd,
+        // Quadrant 1
         OpecodeKind::OP_C_SRLI	    => q1_rd,
         OpecodeKind::OP_C_SRAI	    => q1_rd,
         OpecodeKind::OP_C_ANDI	    => q1_rd,
@@ -96,6 +98,7 @@ pub fn parse_rd(inst: &u32, optype: OpecodeKind) -> u8 {
         OpecodeKind::OP_C_XOR	    => q1_rd,
         OpecodeKind::OP_C_OR	    => q1_rd,
         OpecodeKind::OP_C_AND	    => q1_rd,
+        // Quadrant 2
         OpecodeKind::OP_C_SLLI	    => q2_rd,
         OpecodeKind::OP_C_FLDSP	    => q2_rd,
         OpecodeKind::OP_C_LWSP	    => q2_rd,
@@ -109,17 +112,39 @@ pub fn parse_rd(inst: &u32, optype: OpecodeKind) -> u8 {
     }
 }
 
-pub fn parse_rs1(inst: &u32) -> u8 {
-    let opmap: u8  = (inst & 0x3F) as u8;
-    let rs1: u8 = ((inst >> 15) & 0x1F) as u8;
+pub fn parse_rs1(inst: &u32, opkind:OpecodeKind) -> u8 {
+    let q0_rs1: u8 = ((inst >> 7) & 0x3) as u8;
+    let q1_rs1: u8 = ((inst >> 7) & 0x3) as u8;
+    let q2_rs1: u8 = ((inst >> 7) & 0x3) as u8;
+    let addi_rs1: u8 = ((inst >> 7) & 0x1F) as u8;
 
-    // LUI, AUIPC, JAL, FENCE, ECALL, EBREAK
-    if  opmap == 0b01010111 || opmap == 0b00010111 || 
-        opmap == 0b01101111 || opmap == 0b01110011 { 
-            return 0;
+    match opkind {
+        // Quadrant 0
+        OpecodeKind::OP_C_FLD       => q0_rs1,
+        OpecodeKind::OP_C_FW        => q0_rs1,
+        OpecodeKind::OP_C_FLW       => q0_rs1,
+        OpecodeKind::OP_C_FSD       => q0_rs1,
+        OpecodeKind::OP_C_SW        => q0_rs1,
+        OpecodeKind::OP_C_FSW       => q0_rs1,
+        // Quadrant 1
+        OpecodeKind::OP_C_ADDI		=> addi_rs1,
+        OpecodeKind::OP_C_ADDI16SP	=> addi_rs1,
+        OpecodeKind::OP_C_SRLI		=> q1_rs1,
+        OpecodeKind::OP_C_SRAI		=> q1_rs1,
+        OpecodeKind::OP_C_ANDI		=> q1_rs1,
+        OpecodeKind::OP_C_SUB		=> q1_rs1,
+        OpecodeKind::OP_C_XOR		=> q1_rs1,
+        OpecodeKind::OP_C_OR		=> q1_rs1,
+        OpecodeKind::OP_C_AND		=> q1_rs1,
+        OpecodeKind::OP_C_BEQZ		=> q1_rs1,
+        OpecodeKind::OP_C_BNEZ		=> q1_rs1,
+        // Quadrant 2
+        OpecodeKind::OP_C_SLLI	    => q2_rs1,
+        OpecodeKind::OP_C_JR	    => q2_rs1,
+        OpecodeKind::OP_C_JALR	    => q2_rs1,
+        OpecodeKind::OP_C_ADD	    => q2_rs1,
+        _ => 0,
     }
-
-    return rs1;
 }
 
 pub fn parse_rs2(inst: &u32) -> u8 {
