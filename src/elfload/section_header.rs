7e621513374de1dc805db45f9150e085819ab027
Alignof <neuromancer_wg@outlook.jp>
2021-05-05 01:03:12 +0900

    [update] update the method SectionHeader::section_dump()

--- src/elfload/section_header.rs
+++ src/elfload/section_header.rs
@@ -81,15 +81,18 @@ impl SectionHeader {
 		use crate::decode::Decode;
 
 		println!("--------------------------------");
-		for dump_part in (self.sh_offset .. self.sh_offset + self.sh_size as u32).step_by(4) {
-			if is_cinst(mmap, dump_part as usize) {
-                let mdump = get_u16(mmap, dump_part as usize);
+        let mut dump_head = self.sh_offset;
+        while dump_head != self.sh_offset + self.sh_size {
+			if is_cinst(mmap, dump_head as usize) {
+                let mdump = get_u16(mmap, dump_head as usize);
 				let inst  = mdump.decode();
-				println!("{:<8x}\t{}\t{},{},{}", mdump,inst.opc_to_string(), inst.rd, inst.rs1, inst.rs2);
+				println!("{:<04x}\t\t{}\t\t{},{},{}", mdump,inst.opc_to_string(), inst.rd, inst.rs1, inst.rs2);
+                dump_head += 2;
 			}else{
-                let mdump = get_u32(mmap, dump_part as usize);
+                let mdump = get_u32(mmap, dump_head as usize);
 				let inst  = mdump.decode();
-				println!("{:<08x}\t{}\t{},{},{}", mdump,inst.opc_to_string(), inst.rd, inst.rs1, inst.rs2);
+				println!("{:<08x}\t{}\t\t{},{},{}", mdump,inst.opc_to_string(), inst.rd, inst.rs1, inst.rs2);
+                dump_head += 4;
 			}
 		}
 	}
