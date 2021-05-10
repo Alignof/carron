60899da52bb243ca3ff8f549a4ab845bd088492b
Alignof <neuromancer_wg@outlook.jp>
2021-05-10 09:40:09 +0900

    [update] update the dump format

--- src/elfload/section_header.rs
+++ src/elfload/section_header.rs
@@ -89,7 +89,7 @@ impl SectionHeader {
 				let inst  = mdump.decode();
                 dump_head += 2;
 
-				print!("{:<04x}\t\t{}\t\t{}", mdump, inst.opc_to_string(), inst.reg_to_string());
+				print!("{:<04x}\t\t{:<16}{:>4}", mdump, inst.opc_to_string(), inst.reg_to_string());
                 if let Some(v) = inst.rs1 {print!(" {}", v)}
                 if let Some(v) = inst.rs2 {print!(" {}", v)}
 			}else{
@@ -97,7 +97,7 @@ impl SectionHeader {
 				let inst  = mdump.decode();
                 dump_head += 4;
 
-				print!("{:<08x}\t{}\t\t{}", mdump, inst.opc_to_string(), inst.reg_to_string());
+				print!("{:<08x}\t{:<16}{:>4}", mdump, inst.opc_to_string(), inst.reg_to_string());
                 if let Some(v) = inst.rs1 {print!(" {}", v)}
                 if let Some(v) = inst.rs2 {print!(" {}", v)}
 			}
