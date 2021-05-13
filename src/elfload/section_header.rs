0ab1f93d55bea3a63595f1b7d522bf7de37cd528
Alignof <neuromancer_wg@outlook.jp>
2021-05-13 11:44:37 +0900

    [update] change the function get_section_type_name to mothod of SectionHeader

--- src/elfload/section_header.rs
+++ src/elfload/section_header.rs
@@ -1,31 +1,8 @@
 use super::ElfHeader;
 use crate::elfload::{get_u32, get_u16, is_cinst};
 
-fn get_section_type_name(section_type: u32) -> &'static str {
-	match section_type {
-		0  => "SHT_NULL",
-		1  => "SHT_PROGBITS",
-		2  => "SHT_SYMTAB",
-		3  => "SHT_STRTAB",
-		4  => "SHT_RELA",
-		5  => "SHT_HASH",
-		6  => "SHT_DYNAMIC",
-		7  => "SHT_NOTE",
-		8  => "SHT_NOBITS",
-		9  => "SHT_REL",
-		10 => "SHT_SHLIB",
-		11 => "SHT_DYNSYM",
-		12 => "SHT_LOPROC",
-		13 => "SHT_HIPROC",
-		14 => "SHT_LOUSER",
-		15 => "SHT_HIUSER",
-		_  => "unknown type",
-	}
-}
-
-
 pub struct SectionHeader {
-	sh_name: usize,
+	sh_name: &str,
 	sh_type: u32,
 	sh_flags: u32,
 	sh_addr: u32,
@@ -43,19 +20,19 @@ impl SectionHeader {
 		let mut new_sect = Vec::new();
 
 		for section_num in 0 .. elf_header.e_shnum {
-			let section_start:usize = (elf_header.e_shoff + (elf_header.e_shentsize * section_num) as u32) as usize;
+			let section_head: usize = (elf_header.e_shoff + (elf_header.e_shentsize * section_num) as u32) as usize;
 			new_sect.push(
 				SectionHeader {
-					sh_name:      get_u32(mmap, section_start +  0),
-					sh_type:      get_u32(mmap, section_start +  4),
-					sh_flags:     get_u32(mmap, section_start +  8),
-					sh_addr:      get_u32(mmap, section_start + 12),
-					sh_offset:    get_u32(mmap, section_start + 16),
-					sh_size:      get_u32(mmap, section_start + 20),
-					sh_link:      get_u32(mmap, section_start + 24),
-					sh_info:      get_u32(mmap, section_start + 28),
-					sh_addralign: get_u32(mmap, section_start + 32),
-					sh_entsize:   get_u32(mmap, section_start + 34),
+					sh_name:      get_sh_name(mmap, section_head),
+					sh_type:      get_u32(mmap, section_head +  4),
+					sh_flags:     get_u32(mmap, section_head +  8),
+					sh_addr:      get_u32(mmap, section_head + 12),
+					sh_offset:    get_u32(mmap, section_head + 16),
+					sh_size:      get_u32(mmap, section_head + 20),
+					sh_link:      get_u32(mmap, section_head + 24),
+					sh_info:      get_u32(mmap, section_head + 28),
+					sh_addralign: get_u32(mmap, section_head + 32),
+					sh_entsize:   get_u32(mmap, section_head + 34),
 				}       
 			);
 	    }
@@ -63,13 +40,32 @@ impl SectionHeader {
 		return new_sect;
 	}
 
-    fn nameid_to_str(&self, mmap: &[u8]) -> &str {
+    fn type_to_str(&self) -> &'static str {
+        match self.sh_type {
+            0  => "SHT_NULL",
+            1  => "SHT_PROGBITS",
+            2  => "SHT_SYMTAB",
+            3  => "SHT_STRTAB",
+            4  => "SHT_RELA",
+            5  => "SHT_HASH",
+            6  => "SHT_DYNAMIC",
+            7  => "SHT_NOTE",
+            8  => "SHT_NOBITS",
+            9  => "SHT_REL",
+            10 => "SHT_SHLIB",
+            11 => "SHT_DYNSYM",
+            12 => "SHT_LOPROC",
+            13 => "SHT_HIPROC",
+            14 => "SHT_LOUSER",
+            15 => "SHT_HIUSER",
+            _  => "unknown type",
+        }
     }
 
 	pub fn show(&self, id: usize){
 		println!("============== section header {}==============", id + 1);
-		println!("sh_name:\t{}",	    self.nameid_to_str());
-		println!("sh_type:\t{}",	    get_section_type_name(self.sh_type));
+		println!("sh_name:\t{}",	    self.sh_name);
+		println!("sh_type:\t{}",	    self.type_to_str());
 		println!("sh_flags:\t{}",	    self.sh_flags);
 		println!("sh_addr:\t0x{:x}",	self.sh_addr);
 		println!("sh_offset:\t0x{:x}",	self.sh_offset);
