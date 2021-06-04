79ab7d8de340bd0010d027f9d2f05bf9bf0770f5
Alignof <neuromancer_wg@outlook.jp>
2021-06-04 23:26:36 +0900

    [update] update the overview of Simulator::simulation

--- src/lib.rs
+++ src/lib.rs
@@ -15,5 +15,18 @@ pub struct Simulator {
 impl Simulator {
     pub fn simulation(&self) {
         println!("Simulation...");
+        loop {
+            if is_cinst(mmap, inst_head as usize) {
+                get_u16(mmap, inst_head as usize)
+                    .decode()
+                    .execution();
+                inst_head += 2;
+            }else{
+                get_u32(mmap, inst_head as usize)
+                    .decode()
+                    .execution();
+                inst_head += 4;
+            }
+        }
     }
 } 
