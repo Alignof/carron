53f229609bd56a169403e494ebc1c310055e9e39
Alignof <neuromancer_wg@outlook.jp>
2021-06-09 00:05:03 +0900

    [fix] fix some import and argument type

--- src/cpu/execution.rs
+++ src/cpu/execution.rs
@@ -2,9 +2,9 @@ use super::CPU;
 use super::instruction::Instruction;
 
 pub trait Execution {
-    fn execution(&self, cpu:CPU);
+    fn execution(&self, cpu: &CPU);
 }
 
 impl Execution for Instruction {
-    fn execution(&self, cpu:CPU) {}
+    fn execution(&self, cpu: &CPU) {}
 }
