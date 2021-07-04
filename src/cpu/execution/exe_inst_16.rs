8ef777dd46b15505c503b26fec89774211b39409
Alignof <neuromancer_wg@outlook.jp>
2021-07-04 23:58:48 +0900

    [add] add instruction OP_C_ADDI4SPN

--- src/cpu/execution/exe_inst_16.rs
+++ src/cpu/execution/exe_inst_16.rs
@@ -4,7 +4,9 @@ use crate::cpu::instruction::{Instruction, OpecodeKind};
 pub fn exe_cinst(inst: &Instruction, cpu: &mut CPU) {
     use OpecodeKind::*;
     match inst.opc {
-        OP_C_ADDI4SPN => {},
+        OP_C_ADDI4SPN => {
+            cpu.reg[2] = cpu.reg[2] + ((cpu.reg[inst.imm.unwrap() as usize] >> 2) & 0x1FF);
+        },
         OP_C_FLD      => {},
         OP_C_LW       => {},
         OP_C_FLW      => {},
