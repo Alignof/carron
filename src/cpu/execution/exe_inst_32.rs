02278e3a163e00beffa90f87a31d0044003744e3
Alingof <neuromancer_wg@outlook.jp>
2021-07-21 22:28:51 +0900

    [add] add the instruction OP_SRA

--- src/cpu/execution/exe_inst_32.rs
+++ src/cpu/execution/exe_inst_32.rs
@@ -83,7 +83,10 @@ pub fn exe_inst(inst: &Instruction, cpu: &mut CPU) {
             cpu.reg[inst.rd.unwrap() as usize] =
                 ((cpu.reg[inst.rs1.unwrap() as usize] as u32)  >> cpu.reg[inst.rs2.unwrap() as usize]) as i32;
         },
-        OP_SRA    => {},
+        OP_SRA    => {
+            cpu.reg[inst.rd.unwrap() as usize] =
+                (cpu.reg[inst.rs1.unwrap() as usize] as i32)  >> cpu.reg[inst.rs2.unwrap() as usize];
+        },
         OP_OR     => {
             cpu.reg[inst.rd.unwrap() as usize] =
                 cpu.reg[inst.rs1.unwrap() as usize] | cpu.reg[inst.rs2.unwrap() as usize];
