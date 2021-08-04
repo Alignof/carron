ac9c32db776194a340377e9bfadcee934d10628d
Alignof <neuromancer_wg@outlook.jp>
2021-08-04 09:03:56 +0900

    [add] add Dram::new()

--- src/dram.rs
+++ src/dram.rs
@@ -1,3 +1,14 @@
 pub Dram {
     pub dram: Vec<u8>,
 }
+
+impl Dram {
+    pub fn new() -> Dram {
+        const DRAM_SIZE = 2.pow(16); // 2^16
+        let mut new_dram = vec![0; DRAM_SIZE as usize];
+
+        Dram {
+            dram: new_dram,
+        }
+    }
+}
