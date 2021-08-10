5b1043d963e3d74ef3ae61690c041ae329398665
Alingof <Alignof@outlook.com>
2021-08-10 20:53:17 +0900

    [update] remove mut in new_dram

--- src/bus/dram.rs
+++ src/bus/dram.rs
@@ -5,7 +5,7 @@ pub struct Dram {
 impl Dram {
     pub fn new() -> Dram {
         const DRAM_SIZE: u32 = 65536; // 2^16
-        let mut new_dram = vec![0; DRAM_SIZE as usize];
+        let new_dram = vec![0; DRAM_SIZE as usize];
 
         Dram {
             dram: new_dram,
