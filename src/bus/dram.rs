3c55dcb6400fc7e506509ab0bfefa6db0f215f88
Alingof <Alignof@outlook.com>
2021-08-17 21:20:27 +0900

    [update] update DRAM_SIZE (2^16 -> 2^27)

--- src/bus/dram.rs
+++ src/bus/dram.rs
@@ -4,7 +4,7 @@ pub struct Dram {
 
 impl Dram {
     pub fn new() -> Dram {
-        const DRAM_SIZE: u32 = 65536; // 2^16
+        const DRAM_SIZE: u32 = 1024 * 1024 * 128; // 2^27
         let new_dram = vec![0; DRAM_SIZE as usize];
 
         Dram {
