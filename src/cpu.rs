6308a18115741fa14a96a5de41e8ac3ad5ea82c4
Alingof <Alignof@outlook.com>
2021-08-18 23:31:53 +0900

    [update] remove InstWrapper and impl Deref

--- src/cpu.rs
+++ src/cpu.rs
@@ -2,7 +2,6 @@ pub mod decode;
 pub mod execution;
 mod instruction;
 
-use std::ops::Deref;
 use crate::bus::dram;
 use crate::bus::dram::Dram;
 use crate::cpu::decode::Decode;
@@ -21,32 +20,20 @@ impl CPU {
     }
 }
 
-pub struct InstWrapper {
-    pub raw_inst: Box<dyn Decode>,
-}
-
-impl Deref for InstWrapper {
-    type Target = Box<dyn Decode>;
-
-    fn deref(&self) -> &Self::Target {
-        &self.raw_inst
-    }
-}
-
-pub fn fetch(dram: &dram::Dram, index_pc: usize) -> InstWrapper {
+pub fn fetch(dram: &dram::Dram, index_pc: usize) -> Box<dyn Decode> {
     let is_cinst: bool = Dram::raw_byte(dram, index_pc) & 0x3 != 0x3;
 
     if is_cinst {
         let new_inst: u16 = 
             (Dram::raw_byte(dram, index_pc + 1) as u16) <<  8 |
             (Dram::raw_byte(dram, index_pc + 0) as u16);
-        InstWrapper { raw_inst: Box::new(new_inst) }
+        Box::new(new_inst)
     } else {
         let new_inst: u32 =
             (Dram::raw_byte(dram, index_pc + 3) as u32) << 24 |
             (Dram::raw_byte(dram, index_pc + 2) as u32) << 16 |
             (Dram::raw_byte(dram, index_pc + 1) as u32) <<  8 |
             (Dram::raw_byte(dram, index_pc + 0) as u32);
-        InstWrapper { raw_inst: Box::new(new_inst) }
+        Box::new(new_inst)
     }
 }
