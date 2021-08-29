98248ba2465cfa755f1b5eb689f6d21117057f19
Alingof <Alignof@outlook.com>
2021-08-29 20:41:45 +0900

    [fix] fix the decode of B-type imm

--- src/cpu/decode/mmap_parse_32.rs
+++ src/cpu/decode/mmap_parse_32.rs
@@ -243,7 +243,7 @@ impl Decode for u32 {
         let I_type: i32 = ((inst >> 20) & 0xFFF) as i32;
         let S_type: i32 = ((((inst >> 25) & 0x1F) << 5) | ((inst >> 7) & 0x1F)) as i32;
         let B_type: i32 = ((((inst >> 27) & 0x1) << 11) | (((inst >> 7) & 0x1) << 10) |
-                          (((inst >> 25) & 0x1F) << 4) | ((inst >> 8) & 0xF)) as i32;
+                          (((inst >> 25) & 0x1F) << 4) | ((inst >> 8) & 0xF) << 1) as i32;
         let JAL_imm: i32 = (((((inst >> 12) & 0xFF) << 12) | (((inst >> 20) & 0x1) << 13) |
                           ((inst >> 21) & 0x3FF << 1) | ((inst >> 31) & 0x1 << 31)) << 1) as i32;
 
