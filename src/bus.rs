45223cc58ef4d5f254114d4d392beb7607b6faa8
Alingof <Alignof@outlook.com>
2021-09-19 12:04:20 +0900

    [add] add a method mmu to bus

--- src/bus.rs
+++ src/bus.rs
@@ -1,16 +1,20 @@
 pub mod dram;
+pub mod mmu;
+
 use dram::Dram;
+use mmu::MMU;
 use crate::elfload;
 
 pub struct Bus {
     pub dram: dram::Dram,
-    // todo: add mmu
+    pub mmu: mmu::MMU,
 }
 
 impl Bus {
     pub fn new(loader: elfload::ElfLoader) -> Bus {
         Bus {
             dram: Dram::new(loader),
+            mmu: MMU::new(),
         }
     }
 }
