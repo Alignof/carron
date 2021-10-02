ef89a4dc20cbda2ece88e234c8142a405df1345b
Alingof <Alignof@outlook.com>
2021-10-02 15:02:09 +0900

    [wip][add] add const (PTE|PAGE)SIZE

--- src/bus/mmu.rs
+++ src/bus/mmu.rs
@@ -15,12 +15,17 @@ impl MMU {
     }
 
     fn trans_addr(&self, addr: usize) -> usize {
+        const PTESIZE: usize = 4;
+        const PAGESIZE: usize = 4096; // 2^12
         match self.state {
             AddrTransMode::Bare => addr,
             AddrTransMode::Sv32 => {
                 let VPN1 = addr >> 22 & 0xA;
                 let VPN0 = addr >> 12 & 0xA;
                 let page_off = addr & 0xB;
+
+                // first table walk
+                let PTE_addr = self.ppn * PAGESIZE + VPN1 * PTESIZE;
                 addr
             },
         }
