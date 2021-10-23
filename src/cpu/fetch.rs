8f126765e3f48619840da05086057dd217df8d6e
Alingof <Alignof@outlook.com>
2021-10-23 21:30:44 +0900

    [update] modify method to load/store data in fetch

--- src/cpu/fetch.rs
+++ src/cpu/fetch.rs
@@ -2,21 +2,21 @@ use super::decode::Decode;
 use crate::bus::Device;
 
 pub fn fetch(cpu: &super::CPU) -> Box<dyn Decode> {
-    let dram = &cpu.bus.dram;
+    let bus = &cpu.bus;
     let index_pc: usize = cpu.pc;
-    let is_cinst: bool = dram.raw_byte(index_pc) & 0x3 != 0x3;
+    let is_cinst: bool = bus.raw_byte(index_pc) & 0x3 != 0x3;
 
     if is_cinst {
         let new_inst: u16 = 
-            (dram.raw_byte(index_pc + 1) as u16) <<  8 |
-            (dram.raw_byte(index_pc + 0) as u16);
+            (bus.raw_byte(index_pc + 1) as u16) <<  8 |
+            (bus.raw_byte(index_pc + 0) as u16);
         Box::new(new_inst)
     } else {
         let new_inst: u32 =
-            (dram.raw_byte(index_pc + 3) as u32) << 24 |
-            (dram.raw_byte(index_pc + 2) as u32) << 16 |
-            (dram.raw_byte(index_pc + 1) as u32) <<  8 |
-            (dram.raw_byte(index_pc + 0) as u32);
+            (bus.raw_byte(index_pc + 3) as u32) << 24 |
+            (bus.raw_byte(index_pc + 2) as u32) << 16 |
+            (bus.raw_byte(index_pc + 1) as u32) <<  8 |
+            (bus.raw_byte(index_pc + 0) as u32);
         Box::new(new_inst)
     }
 }
