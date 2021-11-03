936ce8db149a9f833b58eb789f89962f0e53f935
Alingof <Alignof@outlook.com>
2021-11-03 14:19:09 +0900

    [update] make trans_addr to return the raw address if privileged level is the Machine

--- src/bus/mmu.rs
+++ src/bus/mmu.rs
@@ -3,6 +3,7 @@ use std::cell::RefCell;
 use crate::bus::Device;
 use crate::bus::dram::Dram;
 use crate::cpu;
+use crate::cpu::PrivilegedLevel;
 use crate::cpu::csr::CSRname;
 
 pub enum AddrTransMode {
@@ -36,25 +37,31 @@ impl MMU {
             _ => AddrTransMode::Bare,
         };
 
-        match state {
-            AddrTransMode::Bare => addr,
-            AddrTransMode::Sv32 => {
-                let VPN1 = addr >> 22 & 0xA;
-                let VPN0 = addr >> 12 & 0xA;
-                let page_off = addr & 0xB;
+        match *(self.priv_lv.borrow()) {
+            // return raw address if privileged level is Machine
+            PrivilegedLevel::Machine => addr,
+            _ => {
+                match state {
+                    AddrTransMode::Bare => addr,
+                    AddrTransMode::Sv32 => {
+                        let VPN1 = addr >> 22 & 0xA;
+                        let VPN0 = addr >> 12 & 0xA;
+                        let page_off = addr & 0xB;
 
-                // first table walk
-                let PTE_addr = ppn * PAGESIZE + VPN1 * PTESIZE;
-                let PTE = dram.load32(PTE_addr) as usize;
-                let PPN1 = (PTE >> 22 & 0xA) as usize;
+                        // first table walk
+                        let PTE_addr = ppn * PAGESIZE + VPN1 * PTESIZE;
+                        let PTE = dram.load32(PTE_addr) as usize;
+                        let PPN1 = (PTE >> 22 & 0xA) as usize;
 
-                // second table walk
-                let PTE_addr = (PTE >> 10 & 0x16) * PAGESIZE + VPN0 * PTESIZE;
-                let PTE = dram.load32(PTE_addr) as usize;
-                let PPN0 = (PTE >> 12 & 0xA) as usize;
+                        // second table walk
+                        let PTE_addr = (PTE >> 10 & 0x16) * PAGESIZE + VPN0 * PTESIZE;
+                        let PTE = dram.load32(PTE_addr) as usize;
+                        let PPN0 = (PTE >> 12 & 0xA) as usize;
 
-                // return physical address
-                PPN1 << 22 | PPN0 << 12 | page_off
+                        // return physical address
+                        PPN1 << 22 | PPN0 << 12 | page_off
+                    },
+                }
             },
         }
     }
