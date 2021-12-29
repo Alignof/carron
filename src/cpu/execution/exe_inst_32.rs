ecc51430b89e80a441333f9f358896bf27dcbec9
Alingof <Alignof@outlook.com>
2021-12-29 14:54:16 +0900

    [update] update reading xPP in xRET

--- src/cpu/execution/exe_inst_32.rs
+++ src/cpu/execution/exe_inst_32.rs
@@ -225,8 +225,9 @@ pub fn exe_inst(inst: &Instruction, cpu: &mut CPU) {
             cpu.priv_lv = match cpu.csrs.read_xstatus(&cpu.priv_lv, Xstatus::SPP) {
                 0b00 => PrivilegedLevel::User,
                 0b01 => PrivilegedLevel::Supervisor,
+                0b10 => panic!("PrivilegedLevel 0x3 is Reserved."),
                 0b11 => panic!("invalid transition. (S-mode -> M-mode)"),
-                _ => panic!("PrivilegedLevel 0x3 is Reserved."),
+                _ => panic!("invalid PrivilegedLevel"),
             };
             dbg!(&cpu.priv_lv);
             dbg_hex::dbg_hex!(cpu.csrs.read(CSRname::sepc.wrap()));
@@ -243,8 +244,9 @@ pub fn exe_inst(inst: &Instruction, cpu: &mut CPU) {
             cpu.priv_lv = match cpu.csrs.read_xstatus(&cpu.priv_lv, Xstatus::MPP) {
                 0b00 => PrivilegedLevel::User,
                 0b01 => PrivilegedLevel::Supervisor,
+                0b10 => panic!("PrivilegedLevel 0x3 is Reserved."),
                 0b11 => PrivilegedLevel::Machine,
-                _ => panic!("PrivilegedLevel 0x3 is Reserved."),
+                _ => panic!("invalid PrivilegedLevel"),
             };
             let new_pc = cpu.csrs.read(CSRname::mepc.wrap()) as i32;
             cpu.update_pc(new_pc);
