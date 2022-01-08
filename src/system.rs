cb38c0a244bb75111d9591e1f6f0d59b213ba72f
Alingof <Alignof@outlook.com>
2022-01-08 23:21:50 +0900

    [add] add ArgGroup "run option" to app

--- src/system.rs
+++ src/system.rs
@@ -1,4 +1,4 @@
-use clap::arg;
+use clap::{ArgGroup, arg};
 
 #[allow(non_camel_case_types)]
 pub enum ExeOption {
@@ -18,11 +18,19 @@ pub struct Arguments {
 impl Arguments {
     pub fn new(args: &[String]) -> Result<Arguments, &'static str> {
         let app = clap::app_from_crate!()
-            .arg(arg!([filename] "filename of ELF"))
-            .arg(arg!(-d --disasem ...))
-            .arg(arg!(-p --program ...))
-            .arg(arg!(-s --section ...))
-            .arg(arg!(-a --all ...));
+            .arg(arg!([filename] "ELF file").group("ELF"))
+            .arg(arg!(-d --disasem ... "disassemble ELF").requires("ELF"))
+            .arg(arg!(-p --program ... "see add segments").requires("ELF"))
+            .arg(arg!(-s --section ... "see all sections").requires("ELF"))
+            .arg(arg!(-a --all ... "see all data").requires("ELF"))
+            .group(
+                ArgGroup::new("run option")
+                    .required(false)
+                    .args(&["disasem", "program", "section", "all"])
+            )
+            .get_matches();
+
+        dbg!(app);
 
         Ok(Arguments {
             filename: "/opt/riscv32/share/riscv-tests/isa/rv32ui-v-simple".to_string(),
