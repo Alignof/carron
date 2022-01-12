62d2f1345c27a2accb20c791c45fa526921612ef
Alingof <Alignof@outlook.com>
2022-01-12 23:33:39 +0900

    [fix] fix the type of Vec and implement to convert between Vec<u32> and Vec<u8>

--- src/bus/mrom.rs
+++ src/bus/mrom.rs
@@ -7,21 +7,26 @@ pub struct Mrom {
 }
 
 impl Mrom {
-    fn new() -> Mrom {
-        const MROM_SIZE: u32 = 1024 * 1024 * 8; // 2^23
+    pub fn new() -> Mrom {
         let entry_point = 0x80000000;
+        let reset_vector: Vec<u32> = vec![
+            0x00000297, // auipc   t0, 0x0
+            0x02028593, // addi    a1, t0, 32
+            0xf1402573, // csrr    a0, mhartid
+            0x0182a283, // lw      t0, 24(t0)
+            0x00028067, // jr      t0    |
+            0x0,        //               |
+            entry_point,// <-------------+
+            0x0,
+        ];
+
         Mrom {
-            // reset vector
-            mrom: vec![
-                0x00000297, // auipc   t0, 0x0
-                0x02028593, // addi    a1, t0, 32
-                0xf1402573, // csrr    a0, mhartid
-                0x0182a283, // lw      t0, 24(t0)
-                0x00028067, // jr      t0    |
-                0x0,        //               |
-                entry_point,// <-------------+
-                0x0,
-            ],
+            // Vec<u32> -> Vec<u8>
+            mrom: reset_vector
+                .iter()
+                .flat_map(|val| val.to_be_bytes().to_vec())
+                .collect(),
+            // https://github.com/qemu/qemu/blob/b37778b840f6dc6d1bbaf0e8e0641b3d48ad77c5/hw/riscv/virt.c#L47
             base_addr: 0x1000,
         }
     }
