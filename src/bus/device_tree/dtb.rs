be20867e14c010eab73a3bc7f88913b4a78bccad
Alingof <Alignof@outlook.com>
2022-01-28 22:35:01 +0900

    [add] add device_tree/dtb.rs

---
+++ src/bus/device_tree/dtb.rs
@@ -0,0 +1,12 @@
+struct fdt_header {
+    magic: u32,
+    totalsize: u32,
+    off_dt_struct: u32,
+    off_dt_strings: u32,
+    off_mem_rsvmap: u32,
+    version: u32,
+    last_comp_version: u32,
+    boot_cpuid_phys: u32,
+    size_dt_strings: u32,
+    size_dt_struct: u32,
+}
