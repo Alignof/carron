7180e051fbd58a34d81f16cfef7d43867f9474b3
Alingof <Alignof@outlook.com>
2022-02-05 23:27:51 +0900

    [update] remove parse_(node|prop)

--- src/bus/device_tree/dtb/util.rs
+++ src/bus/device_tree/dtb/util.rs
@@ -1,26 +1,12 @@
 use std::iter::Peekable;
 
-fn tokenize<'a>(lines: &'a mut Peekable<std::str::Lines>, errmsg: &'a str)
+pub fn tokenize<'a>(lines: &'a mut Peekable<std::str::Lines>, errmsg: &'a str)
     -> std::str::Split<'a, char> {
     lines 
         .next()
         .expect(errmsg)
         .trim_left() // remove indent
         .split(' ')
-}
-
-pub fn tokenize_node<'a>(lines: &'a mut Peekable<std::str::Lines>)
-    -> std::str::Split<'a, char> {
-    let tokens = tokenize(lines, "node is invalid");
-
-    tokens
-}
-
-pub fn tokenize_prop<'a>(lines: &'a mut Peekable<std::str::Lines>)
-    -> std::str::Split<'a, char> {
-    let tokens = tokenize(lines, "property is invalid");
-
-    tokens
 }
 
 pub fn consume<T: std::cmp::PartialEq, U: std::iter::Iterator + Iterator<Item = T>>
