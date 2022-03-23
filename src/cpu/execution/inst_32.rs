a6fc972408ac7f2763889bc8e9bcbc9bc43f9ec1
Alingof <Alignof@outlook.com>
2022-03-23 22:47:05 +0900

    [add] implement the OP_AMOSWAP_W

--- src/cpu/execution/inst_32.rs
+++ src/cpu/execution/inst_32.rs
@@ -265,6 +265,12 @@ pub fn exe_inst(inst: &Instruction, cpu: &mut CPU) {
             }
 		},
         OP_AMOSWAP_W => {
+            if let Some(load_addr) = cpu.trans_addr(TransFor::Load, cpu.regs.read(inst.rs1)) {
+                cpu.regs.write(inst.rd, cpu.bus.load32(load_addr));
+                let store_addr = cpu.trans_addr(TransFor::Store, cpu.regs.read(inst.rs1))
+                    .expect("transition address failed in AMO");
+                cpu.bus.store32(store_addr, cpu.regs.read(inst.rs2));
+            }
             panic!("not yet implemented: AMOSWAP_W");
 		},
         OP_AMOADD_W => {
