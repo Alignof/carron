00ae8bd52eea8a3a87f053580db29f922c11abf7
Alingof <Alignof@outlook.com>
2022-05-06 00:45:18 +0900

    [fix] fix the data dependencies of OP_CSRR(W|S|C)

--- src/cpu/execution/inst_32/zicsr_extension.rs
+++ src/cpu/execution/inst_32/zicsr_extension.rs
@@ -4,17 +4,19 @@ use crate::cpu::instruction::{Instruction, OpecodeKind};
 pub fn exec(inst: &Instruction, cpu: &mut CPU) {
     match inst.opc {
         OpecodeKind::OP_CSRRW => {
-            // ???
+            let rs1 = cpu.regs.read(inst.rs1) as i32;
             cpu.regs.write(inst.rd, cpu.csrs.read(inst.rs2) as i32);
-            cpu.csrs.write(inst.rs2, cpu.regs.read(inst.rs1));
+            cpu.csrs.write(inst.rs2, rs1);
         },
         OpecodeKind::OP_CSRRS => {
+            let rs1 = cpu.regs.read(inst.rs1) as i32;
             cpu.regs.write(inst.rd, cpu.csrs.read(inst.rs2) as i32);
-            cpu.csrs.bitset(inst.rs2, cpu.regs.read(inst.rs1));
+            cpu.csrs.bitset(inst.rs2, rs1);
         },
         OpecodeKind::OP_CSRRC => {
+            let rs1 = cpu.regs.read(inst.rs1) as i32;
             cpu.regs.write(inst.rd, cpu.csrs.read(inst.rs2) as i32);
-            cpu.csrs.bitclr(inst.rs2, cpu.regs.read(inst.rs1));
+            cpu.csrs.bitclr(inst.rs2, rs1);
         },
         OpecodeKind::OP_CSRRWI => {
             cpu.regs.write(inst.rd, cpu.csrs.read(inst.rs2) as i32);
