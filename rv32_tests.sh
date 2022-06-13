1e2436a592541d95224fdc74dfcc145e7043949e
Alingof <Alignof@outlook.com>
2022-06-13 13:14:40 +0900

    [update] support for multi arch test

--- rv32_tests.sh
+++ rv32_tests.sh
@@ -2,13 +2,19 @@
 
 test_dir=/opt/riscv32/share/riscv-tests/isa/
 test_kinds=(
+    "rv32ui-p"
     "rv32ui-v"
+    "rv32uc-p"
     "rv32uc-v"
 )
 
 for test_kind in ${test_kinds[@]}; do
     for test_name in `ls $test_dir | grep $test_kind | grep -v .dump`; do
-        cargo r $test_dir$test_name > /dev/null 2>&1;
+        if [ ${test_kind: -1} = "p" ]; then
+            cargo r -- --break_point=0x80000044 --result_reg=3 $test_dir$test_name > /dev/null 2>&1;
+        else 
+            cargo r -- --break_point=0xffc02308 --result_reg=10 $test_dir$test_name > /dev/null 2>&1;
+        fi
         result=$?;
 
         ESC=$(printf '\033');
