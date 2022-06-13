4faa90b2414d7be5008494f62bb88f868515657b
Alingof <Alignof@outlook.com>
2022-06-13 11:20:37 +0900

    [update] modify to support multi arch test

--- rv32_tests.sh
+++ rv32_tests.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
 
 test_dir=/opt/riscv32/share/riscv-tests/isa/
-test_kind="rv32uc-v"
+test_kinds=(
+    "rv32ui-v"
+    "rv32uc-v"
+)
 
-for test_name in `ls $test_dir | grep $test_kind | grep -v .dump`; do
-    cargo r $test_dir$test_name > /dev/null 2>&1;
-    result=$?;
+for test_kind in ${test_kinds[@]}; do
+    for test_name in `ls $test_dir | grep $test_kind | grep -v .dump`; do
+        cargo r $test_dir$test_name > /dev/null 2>&1;
+        result=$?;
 
-	ESC=$(printf '\033');
-	if [ $result = 1 ]; then
-		echo "$test_name ${ESC}[32;1m ... passed ${ESC}[m"
-	else
-		echo "$test_name ${ESC}[31;1m ... failed ${ESC}[m"
-	fi
+        ESC=$(printf '\033');
+        if [ $result = 1 ]; then
+            echo "$test_name ${ESC}[32;1m ... passed ${ESC}[m"
+        else
+            echo "$test_name ${ESC}[31;1m ... failed ${ESC}[m"
+        fi
 
+    done;
 done;
