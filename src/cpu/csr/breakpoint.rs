d618862b29e043109eb97bf761c418faba540135
Alingof <Alignof@outlook.com>
2022-07-11 15:31:39 +0900

    [add] add checking breakpoints for each trigger

--- src/cpu/csr/breakpoint.rs
+++ src/cpu/csr/breakpoint.rs
@@ -30,76 +30,77 @@ impl CSRs {
 
 impl CPU {
     pub fn check_breakpoint(&mut self, purpose: &TransFor, addr: u32) -> Result<u32, (Option<i32>, TrapCause, String)> {
-        let tdata1 = self.csrs.read(CSRname::tdata1.wrap())?;
-        let trigger_type = tdata1 >> 28 & 0xF;
+        for trigger_num in 0 .. self.csrs.triggers.tselect + 1 { 
+            let tdata1 = self.csrs.triggers.tdata1[trigger_num];
+            let trigger_type = tdata1 >> 28 & 0xF;
 
-        match trigger_type {
-            0x0 => Ok(addr),
-            0x1 => panic!("SiFive address match trigger is not implemented."),
-            0x2 => {
-                let tdata2 = self.csrs.read(CSRname::tdata2.wrap())?;
-                let match_mode = tdata1 >> 7 & 0xF;
-                /*
-                 * disable privilege check (because user mode stil enable at rv32mi-p)
-                 *
-                let mode_m = tdata1 >> 6 & 0x1;
-                let mode_s = tdata1 >> 4 & 0x1;
-                let mode_u = tdata1 >> 3 & 0x1;
-                
-                if self.priv_lv == PrivilegedLevel::Machine && mode_m == 0x0 ||
-                   self.priv_lv == PrivilegedLevel::Supervisor && mode_s == 0x0 || 
-                   self.priv_lv == PrivilegedLevel::User && mode_u == 0x0 {
-                       return Ok(addr);
-                } 
-                */
+            match trigger_type {
+                0x0 => (),
+                0x1 => panic!("SiFive address match trigger is not implemented."),
+                0x2 => {
+                    let tdata2 = self.csrs.triggers.tdata2[trigger_num];
+                    let match_mode = tdata1 >> 7 & 0xF;
+                    /*
+                     * disable privilege check (because user mode stil enable at rv32mi-p)
+                     *
+                    let mode_m = tdata1 >> 6 & 0x1;
+                    let mode_s = tdata1 >> 4 & 0x1;
+                    let mode_u = tdata1 >> 3 & 0x1;
+                    
+                    if self.priv_lv == PrivilegedLevel::Machine && mode_m == 0x0 ||
+                       self.priv_lv == PrivilegedLevel::Supervisor && mode_s == 0x0 || 
+                       self.priv_lv == PrivilegedLevel::User && mode_u == 0x0 {
+                           return Ok(addr);
+                    } 
+                    */
 
-                if match_mode != 0x0 {
-                    panic!("this match mode is not supported");
-                }
+                    if match_mode != 0x0 {
+                        panic!("this match mode is not supported");
+                    }
 
-                dbg_hex::dbg_hex!(addr);
-                dbg_hex::dbg_hex!(tdata2);
-                dbg_hex::dbg_hex!(tdata1 >> 18 & 0x1);
-                match purpose {
-                    TransFor::Fetch | TransFor::Deleg => {
-                        let fetch_bit = tdata1 >> 2 & 0x1;
-                        if addr == tdata2 && fetch_bit == 1 {
-                            return Err((
-                                Some(addr as i32),
-                                TrapCause::Breakpoint,
-                                "Breakpoint exception (fetch)".to_string()
-                            ))
-                        }
-                        Ok(addr)
-                    },
-                    TransFor::Load => {
-                        let load_bit = tdata1 & 0x1;
-                        if addr == tdata2 && load_bit == 1 {
-                            return Err((
-                                Some(addr as i32),
-                                TrapCause::Breakpoint,
-                                "Breakpoint exception (load)".to_string()
-                            ))
-                        }
-                        Ok(addr)
-                    },
-                    TransFor::StoreAMO => {
-                        let store_bit = tdata1 >> 1 & 0x1;
-                        if addr == tdata2 && store_bit == 1 {
-                            return Err((
-                                Some(addr as i32),
-                                TrapCause::Breakpoint,
-                                "Breakpoint exception (store/AMO)".to_string()
-                            ))
-                        }
-                        Ok(addr)
-                    },
-                }
-            },
-            0x3 => panic!("Instruction count trigger is not implemented."),
-            0x4 => panic!("Interrupt trigger is not implemented."),
-            0x5 => panic!("Exception trigger is not implemented."),
-            _ => panic!("this trigger is not supported: {}", trigger_type),
+                    dbg_hex::dbg_hex!(addr);
+                    dbg_hex::dbg_hex!(tdata2);
+                    dbg_hex::dbg_hex!(tdata1 >> 18 & 0x1);
+                    match purpose {
+                        TransFor::Fetch | TransFor::Deleg => {
+                            let fetch_bit = tdata1 >> 2 & 0x1;
+                            if addr == tdata2 && fetch_bit == 1 {
+                                return Err((
+                                    Some(addr as i32),
+                                    TrapCause::Breakpoint,
+                                    "Breakpoint exception (fetch)".to_string()
+                                ))
+                            }
+                        },
+                        TransFor::Load => {
+                            let load_bit = tdata1 & 0x1;
+                            if addr == tdata2 && load_bit == 1 {
+                                return Err((
+                                    Some(addr as i32),
+                                    TrapCause::Breakpoint,
+                                    "Breakpoint exception (load)".to_string()
+                                ))
+                            }
+                        },
+                        TransFor::StoreAMO => {
+                            let store_bit = tdata1 >> 1 & 0x1;
+                            if addr == tdata2 && store_bit == 1 {
+                                return Err((
+                                    Some(addr as i32),
+                                    TrapCause::Breakpoint,
+                                    "Breakpoint exception (store/AMO)".to_string()
+                                ))
+                            }
+                        },
+                    }
+                },
+                0x3 => panic!("Instruction count trigger is not implemented."),
+                0x4 => panic!("Interrupt trigger is not implemented."),
+                0x5 => panic!("Exception trigger is not implemented."),
+                _ => panic!("this trigger is not supported: {}", trigger_type),
+            }
         }
+
+        Ok(addr)
     }
 }
