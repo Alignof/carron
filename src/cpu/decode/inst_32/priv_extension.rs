c50198a854651f6380154024a8cff797d5a54f61
Alingof <Alignof@outlook.com>
2022-07-20 17:40:29 +0900

    [add] add an instruciton wfi

--- src/cpu/decode/inst_32/priv_extension.rs
+++ src/cpu/decode/inst_32/priv_extension.rs
@@ -3,21 +3,20 @@ use crate::cpu::instruction::OpecodeKind;
 use crate::cpu::TrapCause;
 
 pub fn parse_opecode(inst: u32) -> Result<OpecodeKind, &'static str> {
-    let opmap: u8  = inst.slice(6, 0) as u8;
-    let funct3: u8 = inst.slice(14, 12) as u8;
+    let _opmap: u8  = inst.slice(6, 0) as u8;
+    let _funct3: u8 = inst.slice(14, 12) as u8;
     let funct7: u8 = inst.slice(31, 25) as u8;
 
-    match opmap {
-        0b1110011 => match funct3 {
-            0b000 => match funct7 {
-                0b0001000 => Ok(OpecodeKind::OP_SRET),
-                0b0011000 => Ok(OpecodeKind::OP_MRET),
+    match inst {
+        0b000100000010000000000000011100011 => Ok(OpecodeKind::OP_SRET),
+        0b001100000010000000000000011100011 => Ok(OpecodeKind::OP_MRET),
+        0b000100000101000000000000011100011 => Ok(OpecodeKind::OP_WFI),
+        _ => {
+            match funct7 {
                 0b0001001 => Ok(OpecodeKind::OP_SFENCE_VMA),
                 _ => Err("opecode decoding failed"),
-            },
-            _     => Err("opecode decoding failed"),
+            }
         },
-        _         => Err("opecode decoding failed"),
     }
 }
 
