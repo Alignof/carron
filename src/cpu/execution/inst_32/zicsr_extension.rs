7076a87fb6e854bef767114f3b8855fbb7a82244
Alingof <Alignof@outlook.com>
2022-07-25 14:01:23 +0900

    [update] move check_accessible to zicsr_extensio.rs

--- src/cpu/execution/inst_32/zicsr_extension.rs
+++ src/cpu/execution/inst_32/zicsr_extension.rs
@@ -1,9 +1,63 @@
-use crate::cpu::{CPU, TrapCause};
-use crate::cpu::csr::CSRname;
+use crate::cpu::{CPU, PrivilegedLevel, TrapCause};
+use crate::cpu::csr::{CSRname, Xstatus};
 use crate::cpu::instruction::{Instruction, OpecodeKind};
 
+fn check_accessible(cpu: &mut CPU, dist: usize) -> Result<(), (Option<i32>, TrapCause, String)> {
+    if dist >= 4096 {
+        return Err((
+                Some(cpu.bus.load32(cpu.pc)),
+                TrapCause::IllegalInst,
+                format!("csr size is 4096, but you accessed {}", dist)
+        ));
+    }
+
+    match cpu.priv_lv {
+        PrivilegedLevel::User => {
+            if (0x100 <= dist && dist <= 0x180) || (0x300 <= dist && dist <= 0x344) {
+                    return Err((
+                        Some(cpu.bus.load32(cpu.pc)),
+                        TrapCause::IllegalInst,
+                        format!("You are in User mode but accessed {}", dist)
+                    ));
+                }
+        },
+        PrivilegedLevel::Supervisor => {
+            if 0x300 <= dist && dist <= 0x344 {
+                return Err((
+                    Some(cpu.bus.load32(cpu.pc)),
+                    TrapCause::IllegalInst,
+                    format!("You are in Supervisor mode but accessed {}", dist)
+                ));
+            }
+
+            if dist == CSRname::satp as usize && cpu.csrs.read_xstatus(PrivilegedLevel::Machine, Xstatus::TVM) == 1 {
+                return Err((
+                    Some(cpu.bus.load32(cpu.pc)),
+                    TrapCause::IllegalInst,
+                    format!("mstatsu.TVM == 1 but accessed satp")
+                ));
+            }
+        },
+        _ => (),
+    }
+
+    if 0xc00 <= dist && dist <= 0xc1f {
+        let ctren = cpu.csrs.read(CSRname::mcounteren.wrap())?;
+        if ctren >> (dist - 0xc00) & 0x1 == 1 {
+            return Err((
+                Some(cpu.bus.load32(cpu.pc)),
+                TrapCause::IllegalInst,
+                "mcounteren bit is clear, but attempt reading".to_string()
+            ));
+        }
+    }
+
+    Ok(())
+}
+
 pub fn exec(inst: &Instruction, cpu: &mut CPU) -> Result<(), (Option<i32>, TrapCause, String)> {
-    cpu.csrs.check_accessible(cpu.priv_lv, inst.rs2.unwrap())?;
+    check_accessible(cpu, inst.rs2.unwrap())?;
+
     match inst.opc {
         OpecodeKind::OP_CSRRW => {
             let rs1 = cpu.regs.read(inst.rs1) as i32;
