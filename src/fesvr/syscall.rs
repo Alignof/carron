5346672cff70d3d373972c47b0b753f9d311227a
Alingof <Alignof@outlook.com>
2022-09-06 15:18:57 +0900

    [add] add syscall write

--- src/fesvr/syscall.rs
+++ src/fesvr/syscall.rs
@@ -1,3 +1,7 @@
+use std::io::Write;
+use std::fs::File;
+use std::os::unix::io::FromRawFd;
+
 use crate::CPU;
 
 const RISCV_AT_FDCWD: i32 = -100;
@@ -11,8 +15,12 @@ fn memread(cpu: &CPU, addr: u32, len: u64) -> Vec<u8> {
     buf
 }
 
-
-pub fn write(cpu: &CPU, fd: u64, dst_addr: u64, len: u64) {
-    let buf = memread(cpu, dst_addr as u32, len);
+pub fn write(cpu: &CPU, fd: u64, dst_addr: u64, len: u64) -> Result<i64, std::io::Error> {
     eprintln!("do sys_write(64)");
+    let buf = memread(cpu, dst_addr as u32, len);
+    let mut f = unsafe { File::from_raw_fd(fd as i32) };
+    f.write_all(&buf)?;
+    std::mem::forget(f);
+
+    Ok(len as i64)
 }
