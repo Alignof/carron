28d0a2f5d55f8aaf9bd0af839f157c3cd1e70e3a
Alingof <Alignof@outlook.com>
2022-11-26 15:16:14 +0900

    [update] add pc log diff test to rv32_tests.sh

--- rv32_tests.sh
+++ rv32_tests.sh
@@ -13,21 +13,56 @@ test_kinds=(
     "rv32uc-p"
     "rv32uc-v"
 )
+ESC=$(printf '\033');
+p_filter="0x800000(3c|40|44|48)"
+v_filter="(0xffc021(48|4c|50|54|58))|(0xffc023(04|08))"
+
+diff_output() {
+    if [ ${test_kind: -1} = "p" ]; then
+        filter=$p_filter;
+    else
+        filter=$v_filter;
+    fi;
+
+    cargo r -- $test_dir$test_name 2> /dev/null |
+        perl -ne 'print if /^pc: /' |
+        perl -pe 's/pc: //' |
+        perl -ne "print unless /${filter}/" > ./target/output;
+    spike -l --isa=RV32IMAC $pk_path $test_dir$test_name 2>&1 |
+        perl -pe 's/core.+: //' |
+        perl -pe 's/^: //' |
+        perl -pe 's/ \(0x.+$//' |
+        perl -ne "print unless /${filter}/ or /^\s/ or /exception/" > ./target/expect;
+
+    diff ./target/output ./target/expect
+    if [ $? = 0 ]; then
+        echo "$test_name ${ESC}[32;1m ... passed ${ESC}[m"
+        rm -f ./target/output ./target/expect
+    else
+        echo "$test_name ${ESC}[31;1m ... failed ${ESC}[m";
+        exit 1
+        exit_status=1;
+    fi;
+}
+
+exit_code() {
+    cargo r -- $test_dir$test_name > /dev/null 2>&1;
+    if [ $? = 1 ]; then
+        echo "$test_name ${ESC}[32;1m ... passed ${ESC}[m"
+    else
+        echo "$test_name ${ESC}[31;1m ... failed ${ESC}[m";
+        exit_status=1;
+    fi;
+}
 
 exit_status=0
 for test_kind in ${test_kinds[@]}; do
     for test_name in `ls $test_dir | grep $test_kind | grep -v .dump`; do
-        cargo r -- $test_dir$test_name > /dev/null 2>&1;
-        result=$?;
-
-        ESC=$(printf '\033');
-        if [ $result = 1 ]; then
-            echo "$test_name ${ESC}[32;1m ... passed ${ESC}[m"
+        if [ $(which spike 2> /dev/null) ]; then
+            diff_output;
         else
-            echo "$test_name ${ESC}[31;1m ... failed ${ESC}[m";
-            exit_status=1;
+            exit_code;
         fi
-
     done;
 done;
 
