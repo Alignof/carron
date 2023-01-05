d271ca69baf65ba05168bfaa380c4a625215a52c
Alingof <Alignof@outlook.com>
2023-01-05 14:43:24 +0900

    [!!!][update] remove isa option

--- src/cmdline.rs
+++ src/cmdline.rs
@@ -17,7 +17,6 @@ pub struct Arguments {
     pub exe_option: ExeOption,
     pub pkpath: Option<String>,
     pub init_pc: Option<u64>,
-    pub isa: Option<Isa>,
     pub break_point: Option<u64>,
     pub result_reg: Option<usize>,
     pub main_args: Option<Vec<String>>,
@@ -39,7 +38,6 @@ impl Arguments {
             )
             .arg(arg!(--pk <proxy_kernel> "Run with proxy kernel").required(false))
             .arg(arg!(--pc <init_pc> ... "Set entry address as hex").required(false))
-            .arg(arg!(--isa <isa> ... "Specify ISA").required(false))
             .arg(arg!(--break_point <address> ... "Set break point as hex").required(false))
             .arg(arg!(--result_reg <register_number> ... "Set result register").required(false))
             .arg(arg!(--loglv <log_level> ... "Set log level").required(false))
@@ -75,12 +73,6 @@ impl Arguments {
         let init_pc = app.value_of("pc").map(|x| {
             u64::from_str_radix(x.trim_start_matches("0x"), 16)
                 .expect("invalid pc\nplease set value as hex (e.g. --pc=0x80000000)")
-        });
-
-        let isa = app.value_of("isa").map(|x| match x {
-            "rv32" => Isa::Rv32,
-            "rv64" => Isa::Rv64,
-            _ => Isa::Rv32,
         });
 
         let break_point = app.value_of("break_point").map(|x| {
