434436324eaf01a869bd7c6d93c0cda7d5a21625
Alingof <Alignof@outlook.com>
2023-01-10 14:33:27 +0900

    [add] add fix_to_reg_size to reg

--- src/cpu/reg.rs
+++ src/cpu/reg.rs
@@ -1,21 +1,32 @@
 use crate::cpu::instruction::reg2str;
-use crate::log;
+use crate::{log, Isa};
 
 pub struct Register {
     regs: [u64; 32],
+    isa: Isa,
 }
 
 impl Register {
-    pub fn new() -> Self {
-        Register { regs: [0; 32] }
+    pub fn new(isa: Isa) -> Self {
+        Register { regs: [0; 32], isa }
     }
 
     pub fn show(&self) {
         log::debugln!("=========================================== dump ============================================");
         for (num, reg) in self.regs.iter().enumerate() {
-            log::debug!("{:>4}: 0x{:08x}\t", reg2str(num), reg);
-            if (num + 1) % 4 == 0 {
-                log::debugln!("")
+            match self.isa {
+                Isa::Rv32 => {
+                    log::debug!("{:>4}: 0x{:08x}\t", reg2str(num), reg);
+                    if (num + 1) % 4 == 0 {
+                        log::debugln!("")
+                    }
+                }
+                Isa::Rv64 => {
+                    log::debug!("{:>2}: 0x{:016x}\t", reg2str(num), reg);
+                    if (num + 1) % 3 == 0 {
+                        log::debugln!("")
+                    }
+                }
             }
         }
         log::debugln!("=============================================================================================");
@@ -26,20 +37,28 @@ impl Register {
         if src == 0 {
             0
         } else {
-            self.regs[src]
+            self.fix_to_reg_size(self.regs[src])
         }
     }
 
     pub fn write(&mut self, dist: Option<usize>, src: u64) {
+        let src = self.fix_to_reg_size(src);
         let dist = dist.unwrap();
         if dist != 0 {
             self.regs[dist] = src;
+        }
+    }
+
+    fn fix_to_reg_size(&self, x: u64) -> u64 {
+        match self.isa {
+            Isa::Rv32 => x & 0xffffffff,
+            Isa::Rv64 => x,
         }
     }
 }
 
 impl Default for Register {
     fn default() -> Self {
-        Self::new()
+        Self::new(Isa::Rv64)
     }
 }
