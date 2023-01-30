39bd30e5611556b525d488c0570254788a1a4123
Alingof <Alignof@outlook.com>
2023-01-30 17:13:54 +0900

    [add] implement trap_cause to Mmu

--- src/cpu/mmu.rs
+++ src/cpu/mmu.rs
@@ -1,3 +1,5 @@
+mod pmp;
+
 use crate::bus::dram::Dram;
 use crate::bus::Device;
 use crate::cpu::csr::CSRs;
@@ -41,110 +43,6 @@ impl Mmu {
         };
     }
 
-    fn check_pmp(
-        &self,
-        purpose: TransFor,
-        addr: u64,
-        pmpcfg: u64,
-        pmp_r: u64,
-        pmp_w: u64,
-        pmp_x: u64,
-    ) -> Result<u64, TrapCause> {
-        match purpose {
-            TransFor::Fetch | TransFor::Deleg => {
-                if pmp_x != 1 {
-                    log::debugln!("invalid pmp_x: {:x}", pmpcfg);
-                    return Err(TrapCause::InstPageFault);
-                }
-            }
-            TransFor::Load => {
-                if pmp_r != 1 {
-                    log::debugln!("invalid pmp_r: {:x}", pmpcfg);
-                    return Err(TrapCause::LoadPageFault);
-                }
-            }
-            TransFor::StoreAMO => {
-                if pmp_w != 1 {
-                    log::debugln!("invalid pmp_w: {:x}", pmpcfg);
-                    return Err(TrapCause::StoreAMOPageFault);
-                }
-            }
-        }
-
-        Ok(addr)
-    }
-
-    fn pmp(
-        &self,
-        purpose: TransFor,
-        addr: u64,
-        priv_lv: PrivilegedLevel,
-        csrs: &CSRs,
-    ) -> Result<u64, TrapCause> {
-        let pmpaddrs: Vec<usize> = (0x3B0..0x3BF).collect();
-        let get_pmpcfg = |pmpnum| {
-            let cfgnum = pmpnum / 4;
-            let cfgoff = pmpnum % 4;
-            csrs.read(Some(0x3A0 + cfgnum)).unwrap() >> (4 * cfgoff)
-        };
-
-        for index in 0..pmpaddrs.len() {
-            // pmpaddr0 ~ pmpaddr15
-            let pmpcfg = get_pmpcfg(index);
-            let pmp_r = pmpcfg & 0x1;
-            let pmp_w = pmpcfg >> 1 & 0x1;
-            let pmp_x = pmpcfg >> 2 & 0x1;
-            let pmp_a = pmpcfg >> 3 & 0x3;
-            match pmp_a {
-                0b00 => return Ok(addr),
-                0b01 => {
-                    // TOR
-                    let addr_aligned = addr >> 2; // addr[:2]
-                    if (index == 0 && addr_aligned < csrs.read(Some(pmpaddrs[index])).unwrap())
-                        || (index != 0
-                            && csrs.read(Some(pmpaddrs[index - 1])).unwrap() <= addr_aligned
-                            && addr_aligned < csrs.read(Some(pmpaddrs[index])).unwrap())
-                    {
-                        return self.check_pmp(purpose, addr, pmpcfg, pmp_r, pmp_w, pmp_x);
-                    }
-                }
-                0b10 => {
-                    // NA4
-                    let addr_aligned = addr >> 2; // addr[:2]
-                    if addr_aligned == csrs.read(Some(pmpaddrs[index])).unwrap() {
-                        return self.check_pmp(purpose, addr, pmpcfg, pmp_r, pmp_w, pmp_x);
-                    }
-                }
-                0b11 => {
-                    // NAPOT
-                    let mut addr_aligned = addr >> 2; // addr[:2]
-                    let mut pmpaddr = csrs.read(Some(pmpaddrs[index])).unwrap();
-                    while pmpaddr & 0x1 == 1 {
-                        pmpaddr >>= 1;
-                        addr_aligned >>= 1;
-                    }
-                    pmpaddr >>= 1;
-                    addr_aligned >>= 1;
-
-                    if addr_aligned == pmpaddr {
-                        return self.check_pmp(purpose, addr, pmpcfg, pmp_r, pmp_w, pmp_x);
-                    }
-                }
-                _ => panic!("pmp_a does not matched"),
-            }
-        }
-
-        if priv_lv == PrivilegedLevel::Machine {
-            Ok(addr)
-        } else {
-            Err(match purpose {
-                TransFor::Fetch | TransFor::Deleg => TrapCause::InstPageFault,
-                TransFor::Load => TrapCause::LoadPageFault,
-                TransFor::StoreAMO => TrapCause::StoreAMOPageFault,
-            })
-        }
-    }
-
     fn is_leaf_pte(&self, pte: u64) -> bool {
         let pte_r = pte >> 1 & 0x1;
         let pte_x = pte >> 3 & 0x1;
@@ -156,20 +54,23 @@ impl Mmu {
         let pte_v = pte & 0x1;
         let pte_r = pte >> 1 & 0x1;
         let pte_w = pte >> 2 & 0x1;
-        let trap_cause = |purpose: TransFor| match purpose {
-            TransFor::Fetch => TrapCause::InstPageFault,
-            TransFor::Load => TrapCause::LoadPageFault,
-            TransFor::StoreAMO => TrapCause::StoreAMOPageFault,
-            TransFor::Deleg => TrapCause::InstPageFault,
-        };
 
         // check the PTE validity
         if pte_v == 0 || (pte_r == 0 && pte_w == 1) {
             log::debugln!("invalid pte: {:x}", pte);
-            return Err(trap_cause(purpose));
+            return Err(self.trap_cause(purpose));
         }
 
         Ok(pte)
+    }
+
+    fn trap_cause(&self, purpose: TransFor) -> TrapCause {
+        match purpose {
+            TransFor::Fetch => TrapCause::InstPageFault,
+            TransFor::Load => TrapCause::LoadPageFault,
+            TransFor::StoreAMO => TrapCause::StoreAMOPageFault,
+            TransFor::Deleg => TrapCause::InstPageFault,
+        }
     }
 
     fn check_leaf_pte(
@@ -185,19 +86,19 @@ impl Mmu {
         let pte_u = pte >> 4 & 0x1;
         let pte_a = pte >> 6 & 0x1;
         let pte_d = pte >> 7 & 0x1;
-        let trap_cause = |purpose: TransFor| match purpose {
-            TransFor::Fetch => TrapCause::InstPageFault,
-            TransFor::Load => TrapCause::LoadPageFault,
-            TransFor::StoreAMO => TrapCause::StoreAMOPageFault,
-            TransFor::Deleg => TrapCause::InstPageFault,
-        };
 
         self.check_pte_validity(purpose, pte)?;
 
         // check the U bit
         if pte_u == 0 && priv_lv == PrivilegedLevel::User {
             log::debugln!("invalid pte_u: {:x}", pte);
-            return Err(trap_cause(purpose));
+            return Err(self.trap_cause(purpose));
+        }
+
+        // check the A bit
+        if pte_a == 0 {
+            log::debugln!("invalid pte_a: {:x}", pte);
+            return Err(self.trap_cause(purpose));
         }
 
         match purpose {
@@ -205,15 +106,10 @@ impl Mmu {
                 let sum = csrs.read_xstatus(PrivilegedLevel::Supervisor, Xstatus::SUM);
                 if sum == 0 && pte_u == 1 && priv_lv == PrivilegedLevel::Supervisor {
                     log::debugln!("[SUM] invalid pte_u: {:x}", pte);
-                    return Err(trap_cause(purpose));
+                    return Err(self.trap_cause(purpose));
                 }
             }
             _ => (),
-        }
-
-        if pte_a == 0 {
-            log::debugln!("invalid pte_a: {:x}", pte);
-            return Err(trap_cause(purpose));
         }
 
         // check the PTE field according to translate purpose
@@ -253,12 +149,6 @@ impl Mmu {
         dram: &Dram,
         priv_lv: PrivilegedLevel,
     ) -> Result<u64, TrapCause> {
-        let trap_cause = |purpose: TransFor| match purpose {
-            TransFor::Fetch => TrapCause::InstPageFault,
-            TransFor::Load => TrapCause::LoadPageFault,
-            TransFor::StoreAMO => TrapCause::StoreAMOPageFault,
-            TransFor::Deleg => TrapCause::InstPageFault,
-        };
         // update trans_mode and ppn
         self.update_ppn_and_mode(csrs);
 
@@ -293,7 +183,7 @@ impl Mmu {
                         let PPN0 = if self.is_leaf_pte(PTE) {
                             // check misaligned superpage
                             if (PTE >> 10 & 0x1FF) != 0 {
-                                return Err(trap_cause(purpose)); // exception
+                                return Err(self.trap_cause(purpose)); // exception
                             }
 
                             // check leaf pte and return PPN0
@@ -328,7 +218,7 @@ impl Mmu {
 
                             // check PTE to be leaf
                             if !self.is_leaf_pte(PTE) {
-                                return Err(trap_cause(purpose)); // misaligned superpage
+                                return Err(self.trap_cause(purpose)); // misaligned superpage
                             }
 
                             // check leaf pte and return PPN0
