93cfbf395c28b263814ff7b4cc7745ba5886542a
Alingof <Alignof@outlook.com>
2023-01-31 16:26:25 +0900

    [wip][add] implement Sv39

--- src/cpu/mmu.rs
+++ src/cpu/mmu.rs
@@ -29,7 +29,12 @@ impl Mmu {
     }
 
     fn update_ppn_and_mode(&mut self, csrs: &CSRs) {
-        self.ppn = csrs.read(CSRname::satp.wrap()).unwrap() & 0x3FFFFF;
+        let satp_ppn_mask = match *self.isa {
+            Isa::Rv32 => 0x3FFFFF,
+            Isa::Rv64 => 0xFFFFFFFFFFF,
+        };
+        self.ppn = csrs.read(CSRname::satp.wrap()).unwrap() & satp_ppn_mask;
+
         let satp = csrs.read(CSRname::satp.wrap()).unwrap();
         self.trans_mode = match *self.isa {
             Isa::Rv32 => match satp >> 31 & 0x1 {
@@ -37,7 +42,7 @@ impl Mmu {
                 _ => AddrTransMode::Bare,
             },
             Isa::Rv64 => match satp >> 60 & 0xf {
-                1 => AddrTransMode::Sv39,
+                8 => AddrTransMode::Sv39,
                 _ => AddrTransMode::Bare,
             },
         };
@@ -152,6 +157,8 @@ impl Mmu {
         dram: &Dram,
         priv_lv: PrivilegedLevel,
     ) -> Result<u64, TrapCause> {
+        const PAGESIZE: u64 = 4096; // 2^12
+
         // update trans_mode and ppn
         self.update_ppn_and_mode(csrs);
 
@@ -159,10 +166,8 @@ impl Mmu {
             PrivilegedLevel::Supervisor | PrivilegedLevel::User => {
                 match self.trans_mode {
                     AddrTransMode::Bare => Ok(addr),
-                    AddrTransMode::Sv32 | AddrTransMode::Sv39 => {
+                    AddrTransMode::Sv32 => {
                         const PTESIZE: u64 = 4;
-                        const PAGESIZE: u64 = 4096; // 2^12
-
                         let VPN1 = addr >> 22 & 0x3FF;
                         let VPN0 = addr >> 12 & 0x3FF;
                         let page_off = addr & 0xFFF;
@@ -225,6 +230,77 @@ impl Mmu {
 
                         // check pmp and return transrated address
                         self.pmp(purpose, PPN1 << 22 | PPN0 << 12 | page_off, priv_lv, csrs)
+                    }
+                    AddrTransMode::Sv39 => {
+                        const PTESIZE: u64 = 8;
+                        const PPN_MASK: u64 = 0xFFFFFFFFFFF;
+                        let vpn = vec![addr >> 12 & 0x1FF, addr >> 21 & 0x1FF, addr >> 30 & 0x1FF];
+                        let mut ppn = vpn.clone();
+                        let page_off = addr & 0xFFF;
+                        let pte_offset = vec![10, 19, 28];
+                        let pte_mask = vec![0x1FF, 0x1FF, 0x3FFFFFF];
+                        let mut levels = 2;
+
+                        // first table levels
+                        let mut PTE_addr = self.ppn * PAGESIZE + vpn[levels] * PTESIZE;
+                        log::debugln!("PTE_addr(2): 0x{:x}", PTE_addr);
+                        let mut PTE =
+                            self.check_pte_validity(purpose, dram.load64(PTE_addr).unwrap())?;
+                        log::debugln!("PTE(2): 0x{:x}", PTE);
+                        ppn[levels] = PTE >> pte_offset[levels] & pte_mask[levels];
+                        log::debugln!("PPN2: 0x{:x}", ppn[levels]);
+
+                        // complete the trans addr if PTE is the leaf
+                        while !self.is_leaf_pte(PTE) {
+                            levels -= 1;
+                            PTE_addr = (PTE >> 10 & PPN_MASK) * PAGESIZE + vpn[levels] * PTESIZE;
+                            log::debugln!(
+                                "PTE_addr = (PTE >> 10 & 0xFFFFFFFFFFF) * PAGESIZE + VPN{} * PTESIZE\n\
+                                0x{:x} = 0x{:x} * 0x{:x} + 0x{:x} * 0x{:x}",
+                                levels,
+                                PTE_addr,
+                                (PTE >> 10 & PPN_MASK),
+                                PAGESIZE,
+                                vpn[levels],
+                                PTESIZE
+                            );
+
+                            PTE =
+                                self.check_pte_validity(purpose, dram.load64(PTE_addr).unwrap())?;
+                            log::debugln!("PTE({}): 0x{:x}", levels, PTE);
+
+                            ppn[levels] = PTE >> pte_offset[levels] & pte_mask[levels];
+
+                            if levels == 0 {
+                                return Err(self.trap_cause(purpose)); // exception
+                            }
+                        }
+
+                        // check misaligned superpage
+                        if (PTE >> 10 & 0x1FF) != 0 {
+                            return Err(self.trap_cause(purpose)); // exception
+                        }
+
+                        // check leaf pte and return PPN0
+                        const PTELEAF: usize = 0;
+                        ppn[PTELEAF] = match self.check_leaf_pte(purpose, priv_lv, csrs, PTE) {
+                            Ok(PTE) => PTE >> pte_offset[PTELEAF] & pte_mask[PTELEAF],
+                            Err(cause) => return Err(cause),
+                        };
+
+                        log::debugln!(
+                            "raw address:{:x}\n\t=> transrated address:{:x}",
+                            addr,
+                            ppn[2] << 30 | ppn[1] << 21 | ppn[0] << 12 | page_off
+                        );
+
+                        // check pmp and return transrated address
+                        self.pmp(
+                            purpose,
+                            ppn[2] << 30 | ppn[1] << 21 | ppn[0] << 12 | page_off,
+                            priv_lv,
+                            csrs,
+                        )
                     }
                 }
             }
