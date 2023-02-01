2c6da17185cd19b09f0aa966a73bd73965c9e32a
Alingof <Alignof@outlook.com>
2023-02-01 15:07:34 +0900

    [fix] fix instruction OP_MULHSU

--- src/cpu/execution/inst_32/m_extension.rs
+++ src/cpu/execution/inst_32/m_extension.rs
@@ -30,11 +30,12 @@ pub fn exec(inst: &Instruction, cpu: &mut Cpu) -> Result<(), (Option<u64>, TrapC
                 match *cpu.isa {
                     Isa::Rv32 => ((rs1 as i32 as i64 * rs2 as i32 as i64) >> 32) as u64,
                     Isa::Rv64 => {
-                        if ((rs1 as i64) < 0) == ((rs2 as i64) < 0) {
-                            mulhu_64((rs1 as i64).abs() as u64, (rs2 as i64).abs() as u64)
+                        let rs1 = rs1 as i64;
+                        let rs2 = rs2 as i64;
+                        if (rs1 < 0) == (rs2 < 0) {
+                            mulhu_64(rs1.abs() as u64, rs2.abs() as u64)
                         } else {
-                            !mulhu_64((rs1 as i64).abs() as u64, (rs2 as i64).abs() as u64)
-                                + (rs1 * rs2 == 0) as u64
+                            !mulhu_64(rs1.abs() as u64, rs2.abs() as u64) + (rs1 * rs2 == 0) as u64
                         }
                     }
                 },
@@ -43,7 +44,17 @@ pub fn exec(inst: &Instruction, cpu: &mut Cpu) -> Result<(), (Option<u64>, TrapC
         OpecodeKind::OP_MULHSU => {
             cpu.regs.write(
                 inst.rd,
-                ((rs1 as i32 as i64 * rs2 as u64 as i64) >> 32) as u64,
+                match *cpu.isa {
+                    Isa::Rv32 => ((rs1 as i32 as i64 * rs2 as u64 as i64) >> 32) as u64,
+                    Isa::Rv64 => {
+                        let rs1 = rs1 as i64;
+                        if rs1 < 0 {
+                            !mulhu_64(rs1.abs() as u64, rs2) + (rs1 as u64 * rs2 == 0) as u64
+                        } else {
+                            mulhu_64(rs1.abs() as u64, rs2)
+                        }
+                    }
+                },
             );
         }
         OpecodeKind::OP_MULHU => {
