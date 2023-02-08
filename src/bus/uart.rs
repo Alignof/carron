66b917cd3b4f6210cfdee414f45b5f3f4514b233
Alingof <Alignof@outlook.com>
2023-02-08 15:44:38 +0900

    [add] add UartRegister

--- src/bus/uart.rs
+++ src/bus/uart.rs
@@ -1,8 +1,21 @@
 use super::Device;
 use crate::TrapCause;
+use std::collections::VecDeque;
+
+enum UartRegister {
+    RX_TX,   // read: RX, write: TX
+    IER,     // write: IER
+    IIR_FCR, // read: IIR, write: FCR
+    LCR,     // write: LCR
+    MCR,     // write: MCR
+    LSR,     // read: LSR
+    MSR,     // read: MSR
+    SCR,     // I/O: SCR
+}
 
 pub struct Uart {
     pub uart: Vec<u8>,
+    rx_queue: VecDeque<u8>,
     pub base_addr: u64,
     size: usize,
 }
@@ -20,6 +33,7 @@ impl Uart {
 
         Uart {
             uart: vec![0; UART_SIZE],
+            rx_queue: VecDeque::new(),
             base_addr: 0x1000_0000,
             size: UART_SIZE,
         }
