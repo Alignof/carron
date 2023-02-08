412b36fec2a4e14ac555e4c09a2b9019055c9e57
Alingof <Alignof@outlook.com>
2023-02-08 18:12:54 +0900

    [add] implement rx_byte to Uart

--- src/bus/uart/io.rs
+++ src/bus/uart/io.rs
@@ -1,9 +1,27 @@
 use super::{Uart, UartLsr, UartRegister};
 
 impl Uart {
+    pub fn rx_byte(&mut self) -> u8 {
+        if self.rx_queue.is_empty() {
+            self.uart[UartRegister::LSR as usize] &= !UartLsr::DR.mask();
+            return 0;
+        }
+
+        if self.uart[UartRegister::LSR as usize] & UartLsr::BI.mask() != 0 {
+            self.uart[UartRegister::LSR as usize] &= !(UartLsr::BI.mask());
+            return 0;
+        }
+
+        let front = self.rx_queue.pop_front().unwrap();
+        if self.rx_queue.is_empty() {
+            self.uart[UartRegister::LSR as usize] &= !(UartLsr::DR.mask());
+        }
+
+        front
+    }
+
     pub fn tx_byte(&mut self, data: char) {
-        self.uart[UartRegister::LSR as usize] |=
-            (1 << UartLsr::TEMT as u8) | (1 << UartLsr::THRE as u8);
+        self.uart[UartRegister::LSR as usize] |= (UartLsr::TEMT.mask()) | (UartLsr::THRE.mask());
         print!("{}", char::from_u32(data as u32).unwrap());
     }
 }
