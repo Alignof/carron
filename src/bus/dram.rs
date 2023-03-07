ba87e0aacc9ff0416e8e8a4140e7fd349603fae8
Alingof <Alignof@outlook.com>
2023-03-07 14:59:20 +0900

    [update] update DRAM_SIZE (2^27 -> 2^31)

--- src/bus/dram.rs
+++ src/bus/dram.rs
@@ -14,7 +14,7 @@ pub struct Dram {
 
 impl Dram {
     pub fn new(loader: elfload::ElfLoader, args: &Arguments, isa: Isa) -> Self {
-        const DRAM_SIZE: usize = 1024 * 1024 * 128; // 2^27
+        const DRAM_SIZE: usize = 1024 * 1024 * 1024 * 2; // 2^27
         let virt_entry = loader.get_entry_point().expect("entry point not found.");
 
         // create new dram
