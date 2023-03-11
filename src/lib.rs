831d87834047a7811cde0284e4868749dfb9601b
Alingof <Alignof@outlook.com>
2023-03-11 21:34:52 +0900

    [fix] fix to clear reservation_set when INTERLEAVE

--- src/lib.rs
+++ src/lib.rs
@@ -71,6 +71,7 @@ impl Emulator {
                 }
             }
 
+            self.cpu.reservation_set = None;
             self.cpu.timer_increment(INTERLEAVE / INSNS_PER_RTC_TICK);
         }
     }
