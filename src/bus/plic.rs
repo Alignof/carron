eaa86e0632a09559669fbd9966d6a76828c7f6d2
Alingof <Alignof@outlook.com>
2023-03-21 17:18:46 +0900

    [wip][add] add PlicContext

--- src/bus/plic.rs
+++ src/bus/plic.rs
@@ -14,15 +14,32 @@ const PLIC_MAX_DEVICES: usize = 1024;
 const NDEV: usize = 0x1f;
 const NUM_IDS: usize = NDEV + 1;
 const NUM_IDS_WORD: usize = ((NDEV + 1) + (32 - 1)) / 32;
+const CONTEXT_NUM: usize = 2;
+
+pub struct PlicContext {
+    priority_thresould: u8,
+    enable: Vec<u32>,
+    pending: Vec<u32>,
+    pending_priority: Vec<u8>,
+    claimed: Vec<u32>,
+}
+
+impl PlicContext {
+    fn new() -> Self {
+        PlicContext {
+            priority_thresould: 0,
+            enable: vec![0; PLIC_MAX_DEVICES / 32],
+            pending: vec![0; PLIC_MAX_DEVICES / 32],
+            pending_priority: vec![0; PLIC_MAX_DEVICES],
+            claimed: vec![0; PLIC_MAX_DEVICES / 32],
+        }
+    }
+}
 
 pub struct Plic {
     priority: Vec<u8>,
     level: Vec<u32>,
-    enable: Vec<u32>,
-    pending: Vec<u32>,
-    pending_priority: Vec<u8>,
-    priority_thresould: u8,
-    claimed: Vec<u32>,
+    contexts: Vec<PlicContext>,
     pub mip_mask: u64,
     pub base_addr: u64,
     size: usize,
@@ -40,37 +57,33 @@ impl Plic {
         Plic {
             priority: vec![0; PLIC_MAX_DEVICES],
             level: vec![0; PLIC_MAX_DEVICES],
-            enable: vec![0; PLIC_MAX_DEVICES / 32],
-            pending: vec![0; PLIC_MAX_DEVICES / 32],
-            pending_priority: vec![0; PLIC_MAX_DEVICES],
-            priority_thresould: 0,
-            claimed: vec![0; PLIC_MAX_DEVICES / 32],
+            contexts: vec![PlicContext::new(), PlicContext::new()],
             mip_mask: 0,
             base_addr: 0x0c00_0000,
             size: PLIC_SIZE,
         }
     }
 
-    fn context_best_pending(&self) -> u32 {
+    fn context_best_pending(&self, context_id: usize) -> u32 {
         let mut best_id_prio = 0;
         let mut best_id = 0;
         for i in 0..NUM_IDS_WORD {
-            if self.pending[i] == 0 {
+            if self.contexts[context_id].pending[i] == 0 {
                 continue;
             }
 
             for off in 0..32 {
                 let id = i * 32 + off;
                 if NUM_IDS <= id
-                    || self.pending[i] & (1 << off) == 0
-                    || self.claimed[i] & (1 << off) != 0
+                    || self.contexts[context_id].pending[i] & (1 << off) == 0
+                    || self.contexts[context_id].claimed[i] & (1 << off) != 0
                 {
                     continue;
                 }
 
-                if best_id == 0 || best_id_prio < self.pending_priority[id] {
+                if best_id == 0 || best_id_prio < self.contexts[context_id].pending_priority[id] {
                     best_id = id as u32;
-                    best_id_prio = self.pending_priority[id];
+                    best_id_prio = self.contexts[context_id].pending_priority[id];
                 }
             }
         }
@@ -78,22 +91,22 @@ impl Plic {
         best_id
     }
 
-    fn context_update(&mut self) {
+    fn context_update(&mut self, context_id: usize) {
         const MIP_SEIP_MASK: u64 = 1 << 9;
-        let best_id = self.context_best_pending();
+        let best_id = self.context_best_pending(context_id);
         self.mip_mask = if best_id == 0 { 0 } else { MIP_SEIP_MASK };
     }
 
-    fn context_claim(&mut self) -> u32 {
-        let best_id = self.context_best_pending();
+    fn context_claim(&mut self, context_id: usize) -> u32 {
+        let best_id = self.context_best_pending(context_id);
         let best_id_word = (best_id / 32) as usize;
         let best_id_mask = 1 << (best_id % 32);
 
         if best_id != 0 {
-            self.claimed[best_id_word] |= best_id_mask;
+            self.contexts[context_id].claimed[best_id_word] |= best_id_mask;
         }
 
-        self.context_update();
+        self.context_update(context_id);
         best_id
     }
 
@@ -114,26 +127,26 @@ impl Plic {
         }
     }
 
-    fn context_enable_read(&self, offset: usize) -> u32 {
+    fn context_enable_read(&self, context_id: usize, offset: usize) -> u32 {
         let id_word = (offset >> 2) as usize;
-        if id_word > 0 && id_word < NUM_IDS_WORD {
-            self.enable[id_word] as u32
+        if id_word < NUM_IDS_WORD {
+            self.contexts[context_id].enable[id_word] as u32
         } else {
             0
         }
     }
 
-    fn context_enable_write(&mut self, offset: usize, val: u32) {
+    fn context_enable_write(&mut self, context_id: usize, offset: usize, val: u32) {
         let id_word = (offset >> 2) as usize;
         if id_word >= NUM_IDS_WORD {
             return;
         }
 
-        let old_val = self.enable[id_word];
-        let new_val = if id_word == 0 { val & 0xfffffffe } else { val };
+        let old_val = self.contexts[context_id].enable[id_word];
+        let new_val = if id_word == 0 { val & 0xffff_fffe } else { val };
         let xor_val = old_val ^ new_val;
 
-        self.enable[id_word] = new_val;
+        self.contexts[context_id].enable[id_word] = new_val;
 
         for i in 0..32 {
             let id = id_word * 32 + i;
@@ -145,42 +158,43 @@ impl Plic {
             }
 
             if new_val & id_mask != 0 && self.level[id_word] & id_mask != 0 {
-                self.pending[id_word] |= id_mask;
-                self.pending_priority[id] = id_prio;
+                self.contexts[context_id].pending[id_word] |= id_mask;
+                self.contexts[context_id].pending_priority[id] = id_prio;
             } else if new_val & id_mask == 0 {
-                self.pending[id_word] &= !id_mask;
-                self.pending_priority[id] = 0;
-                self.claimed[id_word] &= !id_mask;
+                self.contexts[context_id].pending[id_word] &= !id_mask;
+                self.contexts[context_id].pending_priority[id] = 0;
+                self.contexts[context_id].claimed[id_word] &= !id_mask;
             }
         }
 
-        self.context_update();
+        self.context_update(context_id);
     }
 
-    fn context_read(&mut self, offset: usize) -> u32 {
+    fn context_read(&mut self, context_id: usize, offset: usize) -> u32 {
         match offset {
-            CONTEXT_THRESHOLD => self.priority_thresould as u32,
-            CONTEXT_CLAIM => self.context_claim(),
+            CONTEXT_THRESHOLD => self.contexts[context_id].priority_thresould as u32,
+            CONTEXT_CLAIM => self.context_claim(context_id),
             _ => unreachable!(),
         }
     }
 
-    fn context_write(&mut self, offset: usize, val: u32) {
+    fn context_write(&mut self, context_id: usize, offset: usize, val: u32) {
         match offset {
             CONTEXT_THRESHOLD => {
                 const PLIC_PRIO_MASK: u32 = 0b1111;
                 let val = val & PLIC_PRIO_MASK;
                 if val <= PLIC_PRIO_MASK {
-                    self.priority_thresould = val as u8;
-                    self.context_update();
+                    self.contexts[context_id].priority_thresould = val as u8;
+                    self.context_update(context_id);
                 }
             }
             CONTEXT_CLAIM => {
                 let id_word = (val / 32) as usize;
                 let id_mask = 1 << (val % 32);
-                if val < NUM_IDS as u32 && self.enable[id_word] & id_mask != 0 {
-                    self.claimed[id_word] &= !id_mask;
-                    self.context_update();
+                if val < NUM_IDS as u32 && self.contexts[context_id].enable[id_word] & id_mask != 0
+                {
+                    self.contexts[context_id].claimed[id_word] &= !id_mask;
+                    self.context_update(context_id);
                 }
             }
             _ => unreachable!("offset: {:#x}", offset),
@@ -202,17 +216,19 @@ impl Plic {
             self.level[id_word] &= !id_mask;
         }
 
-        if self.enable[id_word] & id_mask != 0 {
-            if level != 0 {
-                self.pending[id_word] |= id_mask;
-                self.pending_priority[id] = self.priority[id];
-            } else {
-                self.pending[id_word] &= !id_mask;
-                self.pending_priority[id] = 0;
-                self.claimed[id_word] &= !id_mask;
-            }
+        for c in 0..CONTEXT_NUM {
+            if self.contexts[c].enable[id_word] & id_mask != 0 {
+                if level != 0 {
+                    self.contexts[c].pending[id_word] |= id_mask;
+                    self.contexts[c].pending_priority[id] = self.priority[id];
+                } else {
+                    self.contexts[c].pending[id_word] &= !id_mask;
+                    self.contexts[c].pending_priority[id] = 0;
+                    self.contexts[c].claimed[id_word] &= !id_mask;
+                }
 
-            self.context_update();
+                self.context_update(c);
+            }
         }
     }
 }
@@ -233,7 +249,7 @@ impl Device for Plic {
     fn store8(&mut self, addr: u64, _data: u64) -> Result<(), (Option<u64>, TrapCause, String)> {
         Err((
             Some(addr),
-            TrapCause::StoreAMOPageFault,
+            TrapCause::StoreAMOAccessFault,
             "plic only allows load/store32,64 but try store8".to_string(),
         ))
     }
@@ -241,7 +257,7 @@ impl Device for Plic {
     fn store16(&mut self, addr: u64, _data: u64) -> Result<(), (Option<u64>, TrapCause, String)> {
         Err((
             Some(addr),
-            TrapCause::StoreAMOPageFault,
+            TrapCause::StoreAMOAccessFault,
             "plic only allows load/store32,64 but try store16".to_string(),
         ))
     }
@@ -256,12 +272,28 @@ impl Device for Plic {
             ENABLE_BASE..=CONTEXT_BASE_MINUS_ONE => {
                 let cntx = (addr - ENABLE_BASE) / ENABLE_PER_HART;
                 let addr = addr - (cntx * ENABLE_PER_HART + ENABLE_BASE);
-                Ok(self.context_enable_write(addr, data as u32))
+                if cntx < CONTEXT_NUM {
+                    Ok(self.context_enable_write(cntx, addr, data as u32))
+                } else {
+                    Err((
+                        Some(addr as u64),
+                        TrapCause::StoreAMOAccessFault,
+                        "plic only allows load/store32,64 but try store16".to_string(),
+                    ))
+                }
             }
             CONTEXT_BASE..=PLIC_SIZE_MINUS_ONE => {
                 let cntx = (addr - CONTEXT_BASE) / CONTEXT_PER_HART;
                 let addr = addr - (cntx * CONTEXT_PER_HART + CONTEXT_BASE);
-                Ok(self.context_write(addr, data as u32))
+                if cntx < CONTEXT_NUM {
+                    Ok(self.context_write(cntx, addr, data as u32))
+                } else {
+                    Err((
+                        Some(addr as u64),
+                        TrapCause::StoreAMOAccessFault,
+                        "plic only allows load/store32,64 but try store16".to_string(),
+                    ))
+                }
             }
             _ => unreachable!(),
         }
@@ -276,7 +308,7 @@ impl Device for Plic {
     fn load8(&mut self, addr: u64) -> Result<u64, (Option<u64>, TrapCause, String)> {
         Err((
             Some(addr),
-            TrapCause::LoadPageFault,
+            TrapCause::LoadAccessFault,
             "plic only allows load/store32,64 but try load8".to_string(),
         ))
     }
@@ -284,7 +316,7 @@ impl Device for Plic {
     fn load16(&self, addr: u64) -> Result<u64, (Option<u64>, TrapCause, String)> {
         Err((
             Some(addr),
-            TrapCause::LoadPageFault,
+            TrapCause::LoadAccessFault,
             "plic only allows load/store32,64 but try load16".to_string(),
         ))
     }
@@ -299,12 +331,28 @@ impl Device for Plic {
             ENABLE_BASE..=CONTEXT_BASE_MINUS_ONE => {
                 let cntx = (addr - ENABLE_BASE) / ENABLE_PER_HART;
                 let addr = addr - (cntx * ENABLE_PER_HART + ENABLE_BASE);
-                Ok(self.context_enable_read(addr) as u64)
+                if cntx < CONTEXT_NUM {
+                    Ok(self.context_enable_read(cntx, addr) as u64)
+                } else {
+                    Err((
+                        Some(addr as u64),
+                        TrapCause::LoadAccessFault,
+                        "plic only allows load/store32,64 but try store16".to_string(),
+                    ))
+                }
             }
             CONTEXT_BASE..=PLIC_SIZE_MINUS_ONE => {
                 let cntx = (addr - CONTEXT_BASE) / CONTEXT_PER_HART;
                 let addr = addr - (cntx * CONTEXT_PER_HART + CONTEXT_BASE);
-                Ok(self.context_read(addr) as u64)
+                if cntx < CONTEXT_NUM {
+                    Ok(self.context_read(cntx, addr) as u64)
+                } else {
+                    Err((
+                        Some(addr as u64),
+                        TrapCause::LoadAccessFault,
+                        "plic only allows load/store32,64 but try store16".to_string(),
+                    ))
+                }
             }
             _ => unreachable!(),
         }
@@ -318,7 +366,7 @@ impl Device for Plic {
     fn load_u8(&self, addr: u64) -> Result<u64, (Option<u64>, TrapCause, String)> {
         Err((
             Some(addr),
-            TrapCause::LoadPageFault,
+            TrapCause::LoadAccessFault,
             "plic only allows load/store32,64 but try load_u8".to_string(),
         ))
     }
@@ -326,7 +374,7 @@ impl Device for Plic {
     fn load_u16(&self, addr: u64) -> Result<u64, (Option<u64>, TrapCause, String)> {
         Err((
             Some(addr),
-            TrapCause::LoadPageFault,
+            TrapCause::LoadAccessFault,
             "plic only allows load/store32,64 but try load_u16".to_string(),
         ))
     }
@@ -334,7 +382,7 @@ impl Device for Plic {
     fn load_u32(&self, addr: u64) -> Result<u64, (Option<u64>, TrapCause, String)> {
         Err((
             Some(addr),
-            TrapCause::LoadPageFault,
+            TrapCause::LoadAccessFault,
             "plic only allows load/store32,64 but try load_u32".to_string(),
         ))
     }
