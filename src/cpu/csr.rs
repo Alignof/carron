bb79d45e8696196ea093b17f78d36adf19e5b3c5
Alingof <Alignof@outlook.com>
2023-03-31 23:50:47 +0900

    [wip][add] add CSRs::check_accessible

--- src/cpu/csr.rs
+++ src/cpu/csr.rs
@@ -88,6 +88,158 @@ impl CSRs {
         }
     }
 
+    fn check_accessible(&self, dist: usize) -> Result<(), (Option<u64>, TrapCause, String)> {
+        if dist >= 4096 {
+            return Err((
+                None,
+                TrapCause::IllegalInst,
+                format!("csr size is 4096, but you accessed {dist}"),
+            ));
+        }
+
+        match cpu.priv_lv {
+            PrivilegedLevel::User => {
+                if (0x100..=0x180).contains(&dist) || (0x300..=0x344).contains(&dist) {
+                    return Err((
+                        None,
+                        TrapCause::IllegalInst,
+                        format!("You are in User mode but accessed {dist}"),
+                    ));
+                }
+            }
+            PrivilegedLevel::Supervisor => {
+                if (0x300..=0x344).contains(&dist) {
+                    return Err((
+                        None,
+                        TrapCause::IllegalInst,
+                        format!("You are in Supervisor mode but accessed {dist}"),
+                    ));
+                }
+
+                if dist == CSRname::satp as usize
+                    && self.read_xstatus(PrivilegedLevel::Machine, Xstatus::TVM) == 1
+                {
+                    return Err((
+                        None,
+                        TrapCause::IllegalInst,
+                        "mstatus.TVM == 1 but accessed satp".to_string(),
+                    ));
+                }
+            }
+            _ => (),
+        }
+
+        if (0xc00..=0xc1f).contains(&dist) {
+            match cpu.priv_lv {
+                PrivilegedLevel::Machine | PrivilegedLevel::Reserved => (),
+                PrivilegedLevel::Supervisor => {
+                    let mctren = self.read(CSRname::mcounteren.wrap())?;
+                    if mctren >> (dist - 0xc00) & 0x1 == 0 {
+                        return Err((
+                            None,
+                            TrapCause::IllegalInst,
+                            "mcounteren bit is cleared, but attempt reading".to_string(),
+                        ));
+                    }
+                }
+                PrivilegedLevel::User => {
+                    let mctren = self.read(CSRname::mcounteren.wrap())?;
+                    if mctren >> (dist - 0xc00) & 0x1 == 0 {
+                        return Err((
+                            None,
+                            TrapCause::IllegalInst,
+                            "mcounteren bit is cleared, but attempt reading".to_string(),
+                        ));
+                    }
+                    let sctren = self.read(CSRname::scounteren.wrap())?;
+                    if sctren >> (dist - 0xc00) & 0x1 == 0 {
+                        return Err((
+                            None,
+                            TrapCause::IllegalInst,
+                            "mcounteren bit is cleared, but attempt reading".to_string(),
+                        ));
+                    }
+                }
+            }
+        }
+
+        // riscv-privileged-20190608.pdf p7
+        let csrs_ranges = vec![
+            0x000..=0x0ff,
+            0x400..=0x4ff,
+            0x800..=0x8ff,
+            0xc00..=0xc7f,
+            0xc80..=0xcbf,
+            0xcc0..=0xcff,
+            0x100..=0x1ff,
+            0x500..=0x57f,
+            0x580..=0x5bf,
+            0x5c0..=0x5ff,
+            0x900..=0x97f,
+            0x980..=0x9bf,
+            0x9c0..=0x9ff,
+            0xd00..=0xd7f,
+            //0xd80..=0xdbf,
+            0xdc0..=0xdff,
+            0x200..=0x2ff,
+            0x600..=0x67f,
+            0x680..=0x6bf,
+            0x6c0..=0x6ff,
+            0xa00..=0xa7f,
+            0xa80..=0xabf,
+            0xac0..=0xaff,
+            0xe00..=0xe7f,
+            0xe80..=0xebf,
+            0xec0..=0xeff,
+            //0x300..=0x3ff,
+            0x300..=0x30b, // disable mstateen0(0x30c)
+            0x30d..=0x3bf, // disable pmpaddr16~
+            0x700..=0x77f,
+            0x780..=0x79f,
+            0x7a0..=0x7af,
+            0x7b0..=0x7bf,
+            0x7c0..=0x7ff,
+            0xb00..=0xb7f,
+            0xb80..=0xbbf,
+            0xbc0..=0xbff,
+            0xf00..=0xf7f,
+            //0xf80..=0xfbf,
+            0xfc0..=0xfff,
+        ];
+
+        if csrs_ranges.iter().any(|x| x.contains(&dist)) {
+            match dist {
+                // == depends on privilege ==
+                // scounteren(0x106) only allow higher
+                0x106 => match cpu.priv_lv {
+                    PrivilegedLevel::User => Err((
+                        None,
+                        TrapCause::IllegalInst,
+                        format!("unknown CSR number: {dist}"),
+                    )),
+                    _ => Ok(()),
+                },
+                // stimecmp(0x14d) only supervisor
+                0x14d => match cpu.priv_lv {
+                    PrivilegedLevel::Supervisor => Ok(()),
+                    _ => Err((
+                        None,
+                        TrapCause::IllegalInst,
+                        format!("unknown CSR number: {dist}"),
+                    )),
+                },
+                _ => Ok(()),
+            }
+        } else {
+            // out of range
+            Err((
+                None,
+                TrapCause::IllegalInst,
+                format!("unknown CSR number: {dist}"),
+            ))
+        }
+    }
+
     pub fn bitset(&mut self, dist: Option<usize>, src: u64) {
         let dist = dist.unwrap();
         let mask = self.mask_warl(dist, src.fix2regsz(&self.isa));
