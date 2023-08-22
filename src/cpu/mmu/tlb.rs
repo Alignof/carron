cc87b2012ee8e07d254de0a69af02f6460ad3017
Alignof <alignof@outlook.com>
2023-08-22 15:18:05 +0900

    [update] add tlb_(fetch|load|store)_tags

--- src/cpu/mmu/tlb.rs
+++ src/cpu/mmu/tlb.rs
@@ -1,33 +1,58 @@
+use crate::cpu::TransFor;
+
 const TLB_ENTRIES: usize = 256;
 const PGSHIFT: usize = 12;
 
 #[derive(Clone)]
 struct TlbEntry {
-    _vaddr: u64,
-    paddr: u64,
+    page_addr: u64,
 }
 
 pub struct Tlb {
-    tlb_tags: Vec<u64>,
+    tlb_fetch_tags: Vec<Option<u64>>,
+    tlb_load_tags: Vec<Option<u64>>,
+    tlb_store_tags: Vec<Option<u64>>,
     tlb_data: Vec<Option<TlbEntry>>,
 }
 
 impl Tlb {
     pub fn new() -> Self {
         Tlb {
-            tlb_tags: vec![0; TLB_ENTRIES],
+            tlb_fetch_tags: vec![None; TLB_ENTRIES],
+            tlb_load_tags: vec![None; TLB_ENTRIES],
+            tlb_store_tags: vec![None; TLB_ENTRIES],
             tlb_data: vec![None; TLB_ENTRIES],
         }
     }
 
-    pub fn lookup(&self, vaddr: u64) -> Option<u64> {
+    pub fn lookup(&self, vaddr: u64, purpose: TransFor) -> Option<u64> {
         let vpn = vaddr >> PGSHIFT;
         let index = (vpn % TLB_ENTRIES as u64) as usize;
-        if self.tlb_tags[vpn as usize % TLB_ENTRIES] == vpn {
-            self.tlb_data[index].as_ref().map(|entry| entry.paddr)
+        let tlb_tag = match purpose {
+            TransFor::Fetch | TransFor::Deleg => self.tlb_fetch_tags[index],
+            TransFor::Load => self.tlb_load_tags[index],
+            TransFor::StoreAMO => self.tlb_store_tags[index],
+        };
+        if tlb_tag == Some(vpn) {
+            self.tlb_data[index].as_ref().map(|entry| entry.page_addr)
         } else {
             None
         }
+    }
+
+    pub fn refill_tlb(&mut self, vaddr: u64, paddr: u64, purpose: TransFor) {
+        let index = ((vaddr >> PGSHIFT) % TLB_ENTRIES as u64) as usize;
+        let expected_tag = vaddr >> PGSHIFT;
+        let new_entry = TlbEntry {
+            page_addr: paddr >> PGSHIFT,
+        };
+
+        match purpose {
+            TransFor::Fetch | TransFor::Deleg => self.tlb_fetch_tags[index] = Some(expected_tag),
+            TransFor::Load => self.tlb_load_tags[index] = Some(expected_tag),
+            TransFor::StoreAMO => self.tlb_store_tags[index] = Some(expected_tag),
+        };
+        self.tlb_data[index] = Some(new_entry);
     }
 
     pub fn flush(&mut self) {
@@ -35,34 +60,29 @@ impl Tlb {
         //self.tlb_data.clear();
         //self.tlb_tags = vec![0; TLB_ENTRIES];
         //self.tlb_data = vec![None; TLB_ENTRIES];
-        println!(
-            "before: {}",
-            self.tlb_data.iter().filter(|x| x.is_some()).count()
-        );
+
+        //println!(
+        //    "before: {}",
+        //    self.tlb_data.iter().filter(|x| x.is_some()).count()
+        //);
         crate::log::infoln!("tlb flushed");
 
-        for t in &mut self.tlb_tags {
-            *t = 0;
+        for t in &mut self.tlb_fetch_tags {
+            *t = None;
+        }
+        for t in &mut self.tlb_load_tags {
+            *t = None;
+        }
+        for t in &mut self.tlb_store_tags {
+            *t = None;
         }
         for d in &mut self.tlb_data {
             *d = None;
         }
 
-        println!(
-            "after: {}",
-            self.tlb_data.iter().filter(|x| x.is_some()).count()
-        )
-    }
-
-    pub fn refill_tlb(&mut self, vaddr: u64, paddr: u64) {
-        let index = ((vaddr >> PGSHIFT) % TLB_ENTRIES as u64) as usize;
-        let expected_tag = vaddr >> PGSHIFT;
-        let new_entry = TlbEntry {
-            _vaddr: vaddr,
-            paddr,
-        };
-
-        self.tlb_tags[index] = expected_tag;
-        self.tlb_data[index] = Some(new_entry);
+        //println!(
+        //    "after: {}",
+        //    self.tlb_data.iter().filter(|x| x.is_some()).count()
+        //)
     }
 }
